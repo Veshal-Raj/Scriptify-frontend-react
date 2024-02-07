2a56a80473629180decf21ed9ae03cf1853091be
Veshal Raj <veshalraj1307@gmail.com>
2024-02-07 21:10:26 +0530

    changed color

--- src/components/UI/GoogleAuthButton.tsx
+++ src/components/UI/GoogleAuthButton.tsx
@@ -4,11 +4,26 @@ import GoogleIcon from '@mui/icons-material/Google';
 const GoogleAuthButton = () => {
   return (
     <div>
-    <Button variant="outlined" sx={{ display: 'flex', gap: '8px', alignItems: 'center',mb: 3, width: '100%' }}>
-      <GoogleIcon /> 
-      Sign in with Google
-    </Button>    
-  </div>
+      <Button 
+        variant="outlined" 
+        sx={{ 
+          display: 'flex', 
+          gap: '8px', 
+          alignItems: 'center',
+          mb: 3, 
+          width: '100%',
+          color: 'black', // Set text color to black
+          borderColor: 'black', // Set border color to black
+          '&:hover': { // Change color on hover
+            borderColor: 'black',
+            color: 'black',
+          },
+        }}
+      >
+        <GoogleIcon /> 
+        Sign in with Google
+      </Button>    
+    </div>
   )
 }
 
