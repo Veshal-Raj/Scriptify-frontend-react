9cf6ec3448b68ee5cc1decb867738b02da4cb063
Veshal Raj <veshalraj1307@gmail.com>
2024-02-18 21:26:03 +0530

    add images and some animation in the hero

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -1,54 +1,119 @@
 import { Box, Button, Typography } from "@mui/material";
+import GroupsIcon from '@mui/icons-material/Groups';
+import { AnimatePresence, motion } from 'framer-motion'
+
+
+const imageVariants = {
+  initial: { opacity: 0, scale: 0, x: "0%", y: "0%" },
+  animate: { opacity: 1, scale: 1, x: ["0%", "0%", "0%"], y: ["0%", "0%", "-0%"], transition: { duration: 1.9 } },
+};
+
+  
 
 const LandingHero = () => {
+  
   return (
-    <Box
-      sx={{
-        height: "auto",
-       
-        padding: "100px 100px",
-        background: "-webkit-linear-gradient(45deg, #FFFFF0, #8A2BE2)",
-        color: "#000",
-        "@media (max-width: 600px)": {
-          padding: "40px", // Padding for mobile view
-        },
+    <>
+      <Box
+        sx={{
+          height: "auto",
 
-      }}
-    >
-      <div>
-        <Typography
-          variant="h2"
-          component="h2"
+          padding: "100px 100px",
+          // background: "-webkit-linear-gradient(45deg, #FFFFF0, #8A2BE2)",
+          color: "#000",
+          "@media (max-width: 600px)": {
+            padding: "40px", // Padding for mobile view
+          },
+
+        }}
+      >
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 60 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 1.9 }}
+          >
+            <Typography variant="h2" component="h2">
+              Code, Connect, Create.
+            </Typography>
+            <Typography variant="h4" component="h2" sx={{ padding: '10px' }}>
+              Bringing Together Developers and Community: Where Ideas Flourish and Innovation Thrives!
+            </Typography>
+          </motion.div>
+        </AnimatePresence>
+        <motion.div 
+        
+        drag
+        dragConstraints={{
+          top: -50,
+          left: -50,
+          right: 50,
+          bottom: 50,
+        }}
         >
-          Code, Connect, Create.
-        </Typography>
-        <Typography
-          variant="h4"
-          component="h2"
-          sx={{ padding: '10px'}}
+
+        <Button
+          variant="outlined"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            paddingX: '20px',
+            paddingY: '15px',
+            marginTop: '50px',
+            marginLeft: '10px',
+            borderRadius: '45px',
+            background: "-webkit-linear-gradient(45deg, #FFFFF0, #a06bd1)",
+            color: 'black',
+
+            transition: 'background-color 0.3s, color 0.3s',
+            '&:hover': {
+              
+              background: "-webkit-linear-gradient(45deg, #FFFFF0, #8540c7)",
+              color: 'black',
+            }
+          }}
+          >
+          <GroupsIcon sx={{ margin: '6px' }} /> {/* Icon */}
+          Join the Community
+        </Button>
+          </motion.div>
+
+
+
+
+
+      </Box>
+
+      <div className="flex relative overflow-hidden my-5 ">
+        <motion.div className="absolute top-24 -left-20 right-0 bottom-0 w-1/2 z-10  overflow-hidden " 
+        variants={imageVariants} initial="initial" animate="animate"
+        whileTap={{ scale: 0.9 }}
+          
         >
-          Bringing Together Developers and Community: Where Ideas Flourish and Innovation Thrives!
-        </Typography>
+          <img
+            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'}
+            alt={`Image 1`}
+            className='h-[700px] w-auto object-cover'
+          />
+        </motion.div>
+
+        <motion.div className="abosulte z-1 left-10 right-0  top-0 bottom-0 mx-auto " variants={imageVariants} initial="initial" animate="animate">
+          <img
+            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'}
+            alt={`Image 2`}
+            className='h-[700px] w-full object-cover '
+          />
+        </motion.div>
+        <motion.div className="absolute top-24 -right-10 z-10 " variants={imageVariants} initial="initial" animate="animate" whileTap={{ scale: 0.9 }}>
+          <img
+            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020083770/e37d830e-e204-41a5-b869-844e8d6f92cf.png?auto=format,compress'}
+            alt={`Image 3`}
+            className='h-[700px] w-auto object-cover '
+          />
+        </motion.div>
       </div>
-
-      <Button 
-  variant="outlined" 
-  sx={{ 
-    paddingX: '20px', 
-    paddingY: '15px', 
-    marginTop: '50px', 
-    marginLeft: '10px',
-    borderRadius: '45px', 
-    transition: 'background-color 0.3s, color 0.3s', 
-    '&:hover': {
-      backgroundColor: 'black', 
-      color: 'white', 
-    }
-  }}
->
-  Join the Community
-</Button>
-    </Box>
+    </>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,9 @@ export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{
-        backgroundImage: "-webkit-linear-gradient(85deg, #EAEAEA, #8A2BE2)",
+        backgroundImage: "-webkit-linear-gradient(85deg, #E0D6E9, #C08DE9)",
         backgroundSize: "100% 100%",
+        // backgroundColor: '#E0D6E9',
         borderBottom: "0.1px  #000",
       }}>
         <Toolbar>
@@ -22,7 +23,30 @@ export default function Navbar() {
           <Typography variant="h6" component="div" color="black" sx={{ flexGrow: 1 }}>
             Scriptify
           </Typography>
-          <Button sx={{ color: 'black' }}>Login</Button>
+
+          <Button variant="outlined" sx={{
+            paddingX: '20px',
+            paddingY: '10px',
+            borderRadius: '45px',
+            // backgroundColor: 'black',
+            color: 'black',
+            // transition: 'background-color 0.3s, color 0.3s', 
+            '&:hover': {
+              background: "-webkit-linear-gradient(45deg, #C99DE9, #a06bd1)",
+              color: 'black', 
+            }
+          }}>Sign Up</Button>
+          <Button variant="outlined" sx={{
+            paddingX: '20px',
+            paddingY: '10px',
+            borderRadius: '45px',
+            marginX: '5px',
+            color: 'black',
+            // backgroundColor: '#EAEAEA',
+            background: "-webkit-linear-gradient(45deg, #C99DE9, #a06bd1)",
+            transition: 'background-color 0.3s, color 0.3s', 
+            
+          }}>Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
