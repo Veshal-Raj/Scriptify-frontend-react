81300f1ec89c08f87ec3bfd3d52d48a26277d2d3
Veshal Raj <veshalraj1307@gmail.com>
2024-02-19 19:42:13 +0530

    navbar some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{
-        backgroundImage: "-webkit-linear-gradient(85deg, #E0D6E9, #C08DE9)",
+        backgroundImage: "-webkit-linear-gradient(85deg, #FDFDFE, #D6E4FB)",
         backgroundSize: "100% 100%",
         // backgroundColor: '#E0D6E9',
         borderBottom: "0.1px  #000",
       }}>
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             aria-label="menu"
@@ -19,8 +19,8 @@ export default function Navbar() {
 
           >
             <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" color="black" sx={{ flexGrow: 1 }}>
+          </IconButton> */}
+          <Typography variant="h4" component="h5" color="black" sx={{ flexGrow: 1, paddingLeft: 5, margin: 2  }}>
             Scriptify
           </Typography>
 
