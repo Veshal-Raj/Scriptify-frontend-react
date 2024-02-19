0802d8e1bb6c70a9106a1808c69baead8848c76c
Veshal Raj <veshalraj1307@gmail.com>
2024-02-19 19:42:40 +0530

    landing page sections

---
+++ src/components/LandingSection3.tsx
@@ -0,0 +1,17 @@
+import { Button, Typography } from "@mui/material"
+
+export const LandingSection3 = () => {
+  return (
+    
+<div className="py-40 px-10 flex-col" style={{ backgroundColor: '#020617', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+  <Typography variant="h4" component="h4" className="text-center py-10 text-blue-600 font-semibold uppercase" style={{ fontFamily: 'Arial', letterSpacing: '0em', fontWeight: 'bold' }}>Scriptify Pro</Typography>
+
+  <Typography variant="h2" component="h3" className="text-center text-white">
+    Level up your online credibility with Scriptify now!
+  </Typography>
+  
+  <Button variant="outlined" sx={{ marginTop: '50px', paddingY: '15px', paddingLeft: '30px', paddingRight: '30px'}}>Join Us</Button>
+</div>
+
+  )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,8 @@
+import { Footer } from "../components/Footer"
 import LandingHero from "../components/LandingHero"
+import LandingSection1 from "../components/LandingSection1"
+import LandingSection2 from "../components/LandingSection2"
+import { LandingSection3 } from "../components/LandingSection3"
 import Navbar from "../components/Navbar"
 
 const LandingPage = () => {
@@ -6,6 +10,10 @@ const LandingPage = () => {
     <div>
         <Navbar />
         <LandingHero />
+        <LandingSection1 />
+        <LandingSection2 />
+        <LandingSection3 />
+        <Footer />
       
     </div>
   )
