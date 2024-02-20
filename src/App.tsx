8b974193afc0deb98a377bf31aa1dac5b93dcb6b
Veshal Raj <veshalraj1307@gmail.com>
2024-02-20 20:48:42 +0530

    started rtk

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import Login from "./pages/Login"
 import Signup from "./pages/signup/Signup";
 import LandingPage from "./pages/LandingPage";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import appStore from "./redux/appStore";
+
+
 const queryClient = new QueryClient()
 
 function App() {
 
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/sign-in" element={<Login />} />
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+      <Provider store={appStore}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<Signup />} />
+              <Route path="/" element={<LandingPage />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Provider>
     </>
   )
 }
