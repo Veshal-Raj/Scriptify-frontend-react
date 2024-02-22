c90c5dd78b1cc57221efc8494decf446f46e255b
Veshal Raj <veshalraj1307@gmail.com>
2024-02-22 20:42:55 +0530

    implemented changes regarding to the data in store

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import SideBar from "./UI/SideBar";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const {userData} = useSelector(state => state.user)
+  console.log(userData.role )
   return (
     <>
     <Box sx={{ flexGrow: 1 }}>
@@ -12,12 +15,11 @@ export default function Navbar() {
         borderBottom: "0.1px  #000",
       }}>
         <Toolbar>
-        <SideBar isOpen={true} onClose={false}/>
-
+        {userData.role === 'Admin' && <SideBar isOpen={true} onClose={false}/>}
           <Typography variant="h4" component="h5" color="black" sx={{ flexGrow: 1, paddingLeft: 5, margin: 2 }}>
             Scriptify
           </Typography>
-          <Link to="/sign-up" style={{ textDecoration: 'none' }}>
+          {!userData && <Link to="/sign-up" style={{ textDecoration: 'none' }}>
             <Button variant="outlined" sx={{
               paddingX: '20px',
               paddingY: '10px',
@@ -28,8 +30,8 @@ export default function Navbar() {
                 color: 'black',
               }
             }}>Sign Up</Button>
-          </Link>
-          <Link to='/sign-in' style={{ textDecoration: 'none' }}>
+          </Link>}
+          {!userData && <Link to='/sign-in' style={{ textDecoration: 'none' }}>
             <Button variant="outlined" sx={{
               paddingX: '20px',
               paddingY: '10px',
@@ -39,7 +41,7 @@ export default function Navbar() {
               background: "-webkit-linear-gradient(45deg, #C99DE9, #a06bd1)",
               transition: 'background-color 0.3s, color 0.3s',
             }}>Login</Button>
-          </Link>
+          </Link>}
         </Toolbar>
       </AppBar>
     </Box>
