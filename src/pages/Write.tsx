ee5cd3de89b3e7d6e23a81a496f5d5e33a98d5e5
Veshal Raj <veshalraj1307@gmail.com>
2024-03-04 09:02:29 +0530

    new page added

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,12 +1,29 @@
-import { useState } from "react"
+import { createContext, useState } from "react"
 import { BlogEditor } from "../components/BlogEditor"
 import { PublishForm } from "../components/PublishForm"
 
+const blogStructure = {
+    title: '',
+    banner: '',
+    content: [],
+    tags: [],
+    des: '',
+    author: {personal_info: {}}
+}
+
+export const EditorContext = createContext({})
+
 const Write = () => {
+    const [blog, setBlog ] = useState(blogStructure)
     const [editorState, setEditorState] = useState('editor')
+
+
   return (
     <>
+      <EditorContext.Provider value={{ blog, setBlog, editorState, setEditorState}}>
+
         {editorState === 'editor'? <BlogEditor /> : <PublishForm />}
+      </EditorContext.Provider>
     </>
   )
 }
