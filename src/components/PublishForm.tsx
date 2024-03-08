37f6b1e1929b3007a0bab5f88cf18afd4329aabb
Veshal Raj <veshalraj1307@gmail.com>
2024-03-09 00:18:07 +0530

    converted the publishform to mui

--- src/components/PublishForm.tsx
+++ src/components/PublishForm.tsx
@@ -1,6 +1,6 @@
 import toast, { Toaster } from "react-hot-toast"
 import CloseIcon from '@mui/icons-material/Close';
-import { Key, useContext } from "react";
+import { useContext } from "react";
 import { EditorContext } from "../pages/Write";
 import { useDispatch, useSelector } from "react-redux";
 import { setBlog, setTextEditor } from "../redux/slice/editorSlice";
@@ -8,13 +8,14 @@ import { Tags } from "./Tags";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { initialState } from "../redux/slice/editorSlice";
-
+import { Button, TextField, Typography } from "@mui/material";
+import { motion } from 'framer-motion'
 export const PublishForm = () => {
   const characterLimit = 200;
   const tagLimit = 10;
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const {setEditorState} = useContext(EditorContext)
+  const { setEditorState } = useContext(EditorContext)
   const { userData } = useSelector(state => state.user)
   const authorId = userData._id
   const blog = useSelector((state) => state.editor.blog);
@@ -36,18 +37,18 @@ export const PublishForm = () => {
 
   const handleBlogDesChange = (e) => {
     const input = e.target
-    dispatch(setBlog({...blog, des: input.value }))
+    dispatch(setBlog({ ...blog, des: input.value }))
   }
 
   const handleKeyDown = (e) => {
     if (e.keyCode === 13 || e.keyCode === 188) {
       e.preventDefault()
       const tagfromInput = e.target.value;
-      if (!tagfromInput.length)return toast.error('type something for the tag')
+      if (!tagfromInput.length) return toast.error('type something for the tag')
       console.log(tagfromInput)
-      if (tags.length <=tagLimit) {
+      if (tags.length <= tagLimit) {
         if (!tags.includes(tagfromInput) && tagfromInput.length) {
-          dispatch(setBlog({...blog, tags: [...tags, tagfromInput] }))
+          dispatch(setBlog({ ...blog, tags: [...tags, tagfromInput] }))
         }
       } else {
         toast.error(`You can add max ${tagLimit} tags`)
@@ -60,25 +61,25 @@ export const PublishForm = () => {
     if (e.target.classList.contains("disable")) {
       return
     }
-      if (!blog.title.length) {
-        return toast.error('Write blog title before publishing')
-      }
-      if (!blog.des.length || blog.des.length > characterLimit) {
-        return toast.error(`Description should be within ${characterLimit} characters to publish`)
-      }
-      if (!tags.length) {
-        return toast.error("Enter at least 1 tag before publish")
-      }
+    if (!blog.title.length) {
+      return toast.error('Write blog title before publishing')
+    }
+    if (!blog.des.length || blog.des.length > characterLimit) {
+      return toast.error(`Description should be within ${characterLimit} characters to publish`)
+    }
+    if (!tags.length) {
+      return toast.error("Enter at least 1 tag before publish")
+    }
 
-      const loadingToast = toast.loading("Publishing...")
-      e.target.classList.add('disable')
+    const loadingToast = toast.loading("Publishing...")
+    e.target.classList.add('disable')
 
-      const blogObj = {
-        title: blog.title, banner: blog.banner, des: blog.des, content: blog.content, tags: blog.tags, author: authorId, draft: false
-      }
+    const blogObj = {
+      title: blog.title, banner: blog.banner, des: blog.des, content: blog.content, tags: blog.tags, author: authorId, draft: false
+    }
 
-      axios.post(import.meta.env.VITE_BASE_URL + 'user/create-blog', blogObj )
-      .then(() => { 
+    axios.post(import.meta.env.VITE_BASE_URL + 'user/create-blog', blogObj)
+      .then(() => {
         e.target.classList.remove('disable')
         toast.dismiss(loadingToast);
         toast.success('Published 👍')
@@ -90,7 +91,7 @@ export const PublishForm = () => {
         dispatch(setEditorState(initialState.editorState));
         dispatch(setTextEditor(initialState.textEditor));
 
-      }).catch(( error) => {
+      }).catch((error) => {
         e.target.classList.remove('disable')
         toast.dismiss(loadingToast)
         console.log(error.response.data.message)
@@ -100,48 +101,97 @@ export const PublishForm = () => {
 
   return (
     <div >
-      <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4" >
+      <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
         <Toaster />
-        <button className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
-          onClick={handleCloseEvent}
-        >          
+        <button className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]" onClick={handleCloseEvent}>
           <CloseIcon />
         </button>
 
-        <div className="max-w-[550px] block mx-auto">
-          <p className="text-dark-grey mb-1">Preview</p>
+        <motion.div
+          className="max-w-[550px] block mx-auto"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Typography variant="body1" className="text-dark-grey mb-1">Preview</Typography>
           <div className="w-full aspect-video rounded-lg overflow-hidden bg-gray-50 mt-4">
-          <img src={blog.banner}  />
+            <img src={blog.banner} alt="Blog Banner" />
           </div>
-          <h1 className="text-4xl font-medium mt-2 leading-tight line-clamp-2">{blog.title}</h1>
-          <p className="font-serif line-clamp-2 text-xl leading-7 mt-4">{blog.des}</p>
-            </div>
-          <div className="border-gray-50 lg:border-1 lg:pl-8 max-w-[700px]">
-            <p className="text-gray-400 mb-2 mt-9">Blog Title</p>
-            <input type="text" placeholder="Blog Title" defaultValue={blog.title} className="w-[100%] max-w-[650px] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black "
+          <Typography variant="h3" className="text-xl font-medium mt-2 leading-tight line-clamp-2">{blog.title}</Typography>
+          <Typography variant="body1" className="font-serif line-clamp-2 text-xl leading-7 mt-4">{blog.des}</Typography>
+        </motion.div>
+
+        <motion.div
+          className="border-gray-50 lg:border-1 lg:pl-8 max-w-[700px]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            sx={{ mt: 4 }}
+          >
+            <Typography variant="body1" className="text-gray-400 mb-2 mt-9">Blog Title</Typography>
+            <TextField
+              type="text"
+              placeholder="Blog Title"
+              defaultValue={blog.title}
+              fullWidth
+              variant="outlined"
+              sx={{ marginBottom: '20px' }}
+              className="w-[100%] max-w-[650px] rounded-md  p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black"
               onChange={handleBlogTitleChange}
             />
-            <p className="text-gray-400 mb-2 mt-9">short description about your blog</p>
-            <textarea maxLength={characterLimit} defaultValue={blog.des} className="h-40 max-w-[650px]  resize-none leading-7 w-[100%] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black "
-              onChange={handleBlogDesChange} onKeyDown={handleDesKeyDown}
-            ></textarea>
-            <p className="mt-1 text-gray-400 text-sm text-right">{characterLimit-blog.des.length} characters left</p>
+            <Typography variant="body1" className="text-gray-400 mb-2 mt-9">Short description about your blog</Typography>
+            <TextField
+              maxLength={characterLimit}
+              defaultValue={blog.des}
+              multiline
+              rows={4}
+              fullWidth
+              variant="outlined"
+              className="h-40 max-w-[650px] resize-none leading-7 w-[100%] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black"
+              onChange={handleBlogDesChange}
+              onKeyDown={handleDesKeyDown}
+            />
+            <Typography variant="body1" className="mt-1 text-gray-400 text-sm text-right">{characterLimit - blog.des.length} characters left</Typography>
+          </motion.div>
 
-            <p className="text-gray-400 mb-2 mt-9">Topics - (Helps in searching and ranking your blog post)</p>
-
-            <div className="relative w-[100%] max-w-[650px] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black bg-gray-50 top-0 left-0  py-2 pb-4 ">
-              <input type="text" placeholder="Topic" className="sticky w-[100%] max-w-[650px] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black bg-white top-0 left-0 mb-3 focus:bg-white" 
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            sx={{ mt: 4 }}
+          >
+            <Typography variant="body1" className="text-gray-400 mb-2 mt-9">Topics - (Helps in searching and ranking your blog post)</Typography>
+            <div className="relative w-[100%] max-w-[650px] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black bg-gray-50 top-0 left-0  py-2 pb-4">
+              <TextField
+                type="text"
+                placeholder="Topic"
+                fullWidth
+                variant="outlined"
+                className="sticky w-[100%] max-w-[650px] rounded-md p-4 bg-grey pl-12 border border-grey focus:bg-transparent placeholder:text-black bg-white top-0 left-0 mb-3 focus:bg-white"
                 onKeyDown={handleKeyDown}
               />
-              {tags.map((tag: unknown, i: Key | null | undefined) => {
-                return <Tags tag={tag} tagIndex={i} key={i} />
-              })}
+              {tags.map((tag, i) => (
+                <Tags tag={tag} tagIndex={i} key={i} />
+              ))}
             </div>
-              <p className="mt-1 max-w-[650px] text-gray-400 text-sm text-right">{tagLimit-tags.length} Tags left</p>
-              <button className="whitespace-nowrap bg-blue-500 text-white rounded-full py-3 px-6 text-xl capitalize hover:bg-opacity-80 "
-                onClick={publishBlogFn}
-              >Publish</button>
-          </div>
+            <Typography variant="body1" className="mt-1 max-w-[650px] text-gray-400 text-sm text-right">{tagLimit - tags.length} Tags left</Typography>
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{ px: 3, py: 1, borderRadius: 15}}
+              className="whitespace-nowrap bg-blue-500 text-white rounded-full py-3 px-6 text-xl capitalize hover:bg-opacity-80"
+              onClick={publishBlogFn}
+            >
+              Publish
+            </Button>
+          </motion.div>
+        </motion.div>
       </section>
     </div>
   )
