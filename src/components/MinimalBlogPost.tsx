b21fd9de5aebd715963f604a358021194a101deb
Veshal Raj <veshalraj1307@gmail.com>
2024-03-10 14:08:50 +0530

    trending blog post added

---
+++ src/components/MinimalBlogPost.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { CardContent, Typography } from '@mui/material';
+import { getDay } from '../hooks/date';
+
+const MinimalBlogPost = ({ blog, index }) => {
+    const { publishedAt, title, author: { personal_info: { username } }, blog_id } = blog;
+
+    return (
+        <Link to={`/user/blog/${blog_id}`} className="flex gap-5 mb-4 mx-5 border p-5 max-w-[1000px] rounded-lg">
+            <motion.div
+                initial={{ opacity: 0, y: 25 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -25 }}
+                transition={{ duration: index * 0.7 }}
+                className="flex gap-2 items-center mb-7"
+            >
+                <Typography variant="h1" component="h1" className="text-4xl sm:text-3xl lg:text-5xl font-bold text-grey leading-none">
+                    {index < 10 ? "0" + (index + 1) : index }
+                </Typography>
+            </motion.div>
+            <CardContent className="">
+                <motion.div
+                    initial={{ opacity: 0, y: 25 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -25 }}
+                    transition={{ duration: index * 0.7 }}
+                    className="flex gap-2 items-center mb-7"
+                >
+                    <Typography variant="body1" component="p" className="line-clamp-1">{username}</Typography>
+                    <Typography variant="body1" component="p" className="min-w-fit">{getDay(publishedAt)}</Typography>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, y: 25 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -25 }}
+                    transition={{ duration: index * 0.7 }}
+                >
+                    <Typography variant="h5" className="text-2xl font-medium leading-7 line-clamp-3 sm:line-clamp-2 font-serif">{title}</Typography>
+                </motion.div>
+            </CardContent>
+        </Link>
+    );
+};
+
+export default MinimalBlogPost;
