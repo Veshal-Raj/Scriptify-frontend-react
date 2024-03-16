8b5641c19e997019356c9d93c1dbe765ff38eea4
Veshal Raj <veshalraj1307@gmail.com>
2024-03-16 10:08:46 +0530

    made landing section 2 responsive

--- src/components/LandingSection2.tsx
+++ src/components/LandingSection2.tsx
@@ -5,7 +5,17 @@ const LandingSection2 = () => {
   return (
     <>
     <div className="pt-40 flex justify-center items-center bg-white" >
-    <Typography variant="h3" component="h3" className="text-black">
+    <Typography variant="h3" component="h3" className="text-black"
+      sx={{
+        padding: '10px',
+        '@media (max-width: 600px)': {
+            fontSize: '1.6rem', // Adjust the font size for small screens
+            textAlign: 'center',
+            mx: '5px', // Reduce horizontal margin for small screens
+            py: '10px', // Reduce vertical padding for small screens
+        },
+    }}
+    >
       Connect globally, explore insightful blogs, and be inspired.
     </Typography>
   </div>
