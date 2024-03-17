739324c7c87422400dea866e7ac0535141c30062
Veshal Raj <veshalraj1307@gmail.com>
2024-03-17 14:03:40 +0530

    navbar reponsive, removed the menu from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
 
             <div className="lg:hidden">
 
-            {userRole === 'user' && <UserSideBar  />}
+            {/* {userRole === 'user' && <UserSideBar  />} */}
             </div>
 
             
