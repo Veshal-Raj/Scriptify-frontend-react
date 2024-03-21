88edf197d80e7672448f4c3c79cf05a9c423f666
Veshal Raj <veshalraj1307@gmail.com>
2024-03-21 21:59:00 +0530

    new route added

--- src/routes/AuthorizedOnly.tsx
+++ src/routes/AuthorizedOnly.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Feed } from "../pages/Feed"
 import UserRoutes from "./routeComponents/UserRoutes"
 import Write from "../pages/Write"
+import Profile from "../pages/Profile"
 
 
 const AuthorizedOnly = () => {
@@ -11,6 +12,7 @@ const AuthorizedOnly = () => {
             <Route element={<UserRoutes />} >
                 <Route path="/feed" element={<Feed />} />
                 <Route path="/write" element={<Write />}/>
+                <Route path="/:id" element={<Profile />}/>
             </Route>
         </Routes>
     </>
