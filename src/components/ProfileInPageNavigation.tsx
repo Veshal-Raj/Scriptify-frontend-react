c4da496f0b37cdc8d7cdca7b9f5c904a8e418b8f
Veshal Raj <veshalraj1307@gmail.com>
2024-03-25 09:14:42 +0530

    profileInPageNavigation introduced

---
+++ src/components/ProfileInPageNavigation.tsx
@@ -0,0 +1,39 @@
+import { Button, ButtonGroup, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+
+const ProfileInPageNavigation = ({ setActiveTab }) => {
+    const [selectedTab, setSelectedTab] = useState("blogPublished");
+    const isMediumScreen = useMediaQuery('(min-width:960px)');
+  
+    const handleTabChange = (tab) => {
+      setActiveTab(tab);
+      setSelectedTab(tab);
+    };
+  
+    return (
+      <ButtonGroup variant="text" aria-label="navigation" className="mt-4 " sx={{ borderColor: "transparent" }}>
+        <Button
+          onClick={() => handleTabChange("blogPublished")}
+          sx={{ bgcolor: selectedTab === "blogPublished" ? '#F3F4F6' : 'transparent' }}
+        >
+          Blog Published
+        </Button>
+        <Button
+          onClick={() => handleTabChange("savedBlogs")}
+          sx={{ bgcolor: selectedTab === "savedBlogs" ? '#F3F4F6' : 'transparent' }}
+        >
+          Saved Blogs
+        </Button>
+        {!isMediumScreen && (
+          <Button
+            onClick={() => handleTabChange("about")}
+            sx={{ bgcolor: selectedTab === "about" ? '#F3F4F6' : 'transparent' }}
+          >
+            About
+          </Button>
+        )}
+      </ButtonGroup>
+    );
+  };
+  
+  export default ProfileInPageNavigation;
