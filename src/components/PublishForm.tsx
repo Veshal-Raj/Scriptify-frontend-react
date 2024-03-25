d7faa71f9def050310b4703505fb850065516bc9
Veshal Raj <veshalraj1307@gmail.com>
2024-03-25 09:15:36 +0530

    added mobilefooter for publishform

--- src/components/PublishForm.tsx
+++ src/components/PublishForm.tsx
@@ -101,7 +101,7 @@ export const PublishForm = () => {
   return (
     <>
     <div >
-      <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
+      <section className="w-screen min-h-screen mb-12 p-4 grid items-center lg:grid-cols-2 py-16 lg:gap-4">
         <Toaster />
         <button className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]" onClick={handleCloseEvent}>
           <CloseIcon />
