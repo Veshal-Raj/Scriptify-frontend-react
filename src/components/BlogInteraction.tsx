8ac6a738a37e25839f6d9461b4543f2cee40f6d3
Veshal Raj <veshalraj1307@gmail.com>
2024-03-27 18:54:31 +0530

    Update: added save and report

--- src/components/BlogInteraction.tsx
+++ src/components/BlogInteraction.tsx
@@ -1,11 +1,15 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { BlogContext } from "./ShowBlogContent"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CommentIcon from '@mui/icons-material/Comment';
-import { IconButton, Tooltip } from "@mui/material";
+import { IconButton, List, ListItem, ListItemIcon, ListItemText, Paper, Popover, Tooltip } from "@mui/material";
 import { Link } from "react-router-dom";
 import XIcon from '@mui/icons-material/X';
 import { useSelector } from "react-redux";
+import ForumIcon from '@mui/icons-material/Forum';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
 
 
 const BlogInteraction = () => {
@@ -15,6 +19,17 @@ const BlogInteraction = () => {
 
     const userId = userData._id
     // console.log(_id,'--------------')
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
     
 
     const isSameUser = userId === authorId
@@ -28,19 +43,27 @@ const BlogInteraction = () => {
             <div className="flex gap-6 justify-between">
                 <div>
                     <div className="flex gap-3 items-center">
-                        <Tooltip title="Like" placement="top">
+                        <Tooltip title="Like" placement="left">
                             <IconButton>
                                 <FavoriteBorderIcon />
                             </IconButton>
                         </Tooltip>
                         <p className="text-xl text-gray-600">{total_likes}</p>
 
-                        <Tooltip title="Comment" placement="top">
+                        <Tooltip title="Comment" placement="bottom">
                             <IconButton>
                                 <CommentIcon />
                             </IconButton>
                         </Tooltip>
                         <p className="text-xl text-gray-600">{total_comments}</p>
+
+                    {!isSameUser &&<Tooltip title="Chat" placement="right">
+                        <IconButton>
+                        <ForumIcon />
+                        </IconButton>
+                    </Tooltip>}
+
+                        
                     </div>
                 </div>
 
@@ -53,6 +76,43 @@ const BlogInteraction = () => {
                             </Link>
                         </div>
                     </Tooltip>
+                    {!isSameUser &&
+                        <>
+                            <IconButton onClick={handlePopoverOpen}>
+                                <MoreVertRoundedIcon />
+                            </IconButton>
+                            <Popover
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handlePopoverClose}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'center',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                }}
+                            >
+                                <Paper>
+                                    <List>
+                                        <ListItem button>
+                                            <ListItemIcon>
+                                                <BookmarkBorderIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Save" />
+                                        </ListItem>
+                                        <ListItem button>
+                                            <ListItemIcon>
+                                                <ReportGmailerrorredIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Report" />
+                                        </ListItem>
+                                    </List>
+                                </Paper>
+                            </Popover>
+                        </>
+                    }
                 </div>
             </div>
 
