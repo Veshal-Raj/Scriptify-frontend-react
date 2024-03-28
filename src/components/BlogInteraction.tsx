cfa27849fd758e222bc26a50a9c28dabba806e69
Veshal Raj <veshalraj1307@gmail.com>
2024-03-28 10:01:05 +0530

    feat: added blog interactions querys and mutations

--- src/components/BlogInteraction.tsx
+++ src/components/BlogInteraction.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { BlogContext } from "./ShowBlogContent"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CommentIcon from '@mui/icons-material/Comment';
@@ -10,16 +10,81 @@ import ForumIcon from '@mui/icons-material/Forum';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
 import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { initialLikeApi, likeBlogApi, unLikeBlogApi } from "../api/user";
+import toast from "react-hot-toast";
 
 
 const BlogInteraction = () => {
-    const { singleBlogData: {title, blog_id, activity, activity: { total_likes, total_comments }, author: { personal_info: { username: author_username }, _id: authorId } },
-    setSingleBlogData } = useContext(BlogContext)
+    const { singleBlogData: { title, _id, blog_id, activity, activity: { total_likes, total_comments }, author: { personal_info: { username: author_username }, _id: authorId } },
+        setSingleBlogData } = useContext(BlogContext)
     const { userData } = useSelector(state => state.user)
 
-    const userId = userData._id
-    // console.log(_id,'--------------')
+    const [isLiked, setIsLiked] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
+
+    const userId = userData._id
+    
+    const blogId = _id
+   
+
+
+    const { data: initialBlogQuery } = useQuery({
+        queryKey: ["initialQuery"],
+        queryFn: () => initialLikeApi(userId, blogId)
+    })
+
+    useEffect(() => {
+        
+        initialBlogQuery
+    }, [])
+
+    console.log('first,,,,,,,,,,,', initialBlogQuery)
+    
+    useEffect(() => {
+        
+        if (initialBlogQuery?.data.response) {
+            setIsLiked(true); 
+        } else {
+            setIsLiked(false); 
+        }
+    }, [initialBlogQuery?.data]);
+
+    const { mutate: likeBlog } = useMutation({
+        mutationFn: likeBlogApi,
+        onSuccess: () => {
+            setIsLiked(true)
+            setSingleBlogData(prevState => ({
+                ...prevState,
+                activity: {
+                    ...prevState.activity,
+                    total_likes: prevState.activity.total_likes + 1
+                }
+            }));
+        },
+        onError:  () => {
+            toast.error('Something went wrong, Please try again later.')
+        }
+    })
+
+    const { mutate: unLikeBlog } = useMutation({
+        mutationFn: unLikeBlogApi,
+        onSuccess: () => {
+            setIsLiked(false)
+            setSingleBlogData(prevState => ({
+                ...prevState,
+                activity: {
+                    ...prevState.activity,
+                    total_likes: prevState.activity.total_likes - 1
+                }
+            }));
+        },
+        onError:  () => {
+            toast.error('Something went wrong, Please try again later.')
+        }
+    })
+
 
     const handlePopoverOpen = (event) => {
         setAnchorEl(event.currentTarget);
@@ -30,12 +95,29 @@ const BlogInteraction = () => {
     };
 
     const open = Boolean(anchorEl);
-    
+
 
     const isSameUser = userId === authorId
-    // console.log(userId, '---', authorId)
-    // console.log(isSameUser)
     
+
+    const handleLike = () => {
+       
+        const data = {
+            blogId: blogId,
+            userId: userId
+        }
+        likeBlog(data)
+    }
+
+    const handleUnLike = () => {
+    
+        const data = {
+            blogId: blogId,
+            userId: userId
+        }
+        unLikeBlog(data)
+    }
+
     return (
         <>
             <hr className="border-gray-200 my-2" />
@@ -43,11 +125,20 @@ const BlogInteraction = () => {
             <div className="flex gap-6 justify-between">
                 <div>
                     <div className="flex gap-3 items-center">
-                        <Tooltip title="Like" placement="left">
-                            <IconButton>
-                                <FavoriteBorderIcon />
-                            </IconButton>
-                        </Tooltip>
+                        {!isLiked ? <>
+                            <Tooltip title="Like" placement="left">
+                                <IconButton onClick={handleLike}>
+                                    <FavoriteBorderIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </> : <>
+                            <Tooltip title="Unlike" placement="left">
+                                <IconButton onClick={handleUnLike}>
+                                    <FavoriteIcon color="warning" />
+                                </IconButton>
+                            </Tooltip>
+                        </>
+                        }
                         <p className="text-xl text-gray-600">{total_likes}</p>
 
                         <Tooltip title="Comment" placement="bottom">
@@ -57,22 +148,22 @@ const BlogInteraction = () => {
                         </Tooltip>
                         <p className="text-xl text-gray-600">{total_comments}</p>
 
-                    {!isSameUser &&<Tooltip title="Chat" placement="right">
-                        <IconButton>
-                        <ForumIcon />
-                        </IconButton>
-                    </Tooltip>}
+                        {!isSameUser && <Tooltip title="Chat" placement="right">
+                            <IconButton>
+                                <ForumIcon />
+                            </IconButton>
+                        </Tooltip>}
 
-                        
+
                     </div>
                 </div>
 
                 <div className="flex gap-6 items-center">
-                {isSameUser && <Link to={`/editor/${blog_id}`} className="hover:text-blue-600">Edit</Link>}
+                    {isSameUser && <Link to={`/editor/${blog_id}`} className="hover:text-blue-600">Edit</Link>}
                     <Tooltip title="Twitter" placement="top">
                         <div>
                             <Link to={`https://twitter.com/intent/tweet?text=Read${title}&url=${location.href}`}>
-                                <XIcon  />
+                                <XIcon />
                             </Link>
                         </div>
                     </Tooltip>
