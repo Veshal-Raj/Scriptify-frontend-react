d45ca85793ceec83286217b5d4b3e23ded6dc3f5
Veshal Raj <veshalraj1307@gmail.com>
2024-04-01 23:34:41 +0530

    update: changed into hook

---
+++ src/hooks/useAws.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const uploadImage = async (img: string) => {
+    let imageUrl = null;
+
+    await axios.get(import.meta.env.VITE_BASE_URL + 'user/get-upload-url')
+    .then( async ({ data: {uploadURL}}) => {
+
+        await axios({
+            method: 'PUT',
+            url: uploadURL,
+            headers: {'Content-Type': 'multipart/form-data'},
+            data: img
+        })
+        .then(() => imageUrl = uploadURL.split('?')[0])
+    })
+
+    return imageUrl
+}
