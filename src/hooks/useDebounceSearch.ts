a509ce5aeda3671d7f26e665bafe44476118bff8
Veshal Raj <veshalraj1307@gmail.com>
2024-04-01 23:34:05 +0530

    update: changed into hook

---
+++ src/hooks/useDebounceSearch.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+
+
+const useDebounce = (value: string, delay: number, searchTerm, setSuggestions) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    const searchCache = useSelector((store) => store.search)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            if (searchCache[searchTerm]) {
+                console.log(searchCache[searchTerm])
+                setSuggestions(searchCache[searchTerm])
+            } else {
+
+                setDebouncedValue(value)
+            }
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+
+    return debouncedValue
+}
+
+export default useDebounce
