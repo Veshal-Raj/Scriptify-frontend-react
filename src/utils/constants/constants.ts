7d454189add52c58e528d99000da3ab0950899a2
Veshal Raj <veshalraj1307@gmail.com>
2024-04-04 00:15:42 +0530

    update: added more constant values

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,3 +1,13 @@
 export const characterLimit = 200;
 export const tagLimit = 10;
 
+export const benefits = [
+    "Access to exclusive live Q&A sessions with authors.",
+    "Early access to unpublished articles and blog posts.",
+    "Customized recommendations tailored to your interests.",
+    "Exclusive access to monthly giveaways and contests.",
+    "Priority support for all inquiries.",
+    "Access to premium in-depth content."
+];
+
+export const personAvatarUrl = 'https://source.unsplash.com/100x100/?person';
