a2b7a75da5a8f14dd9ffd6d4a03cace4cdf493fd
Veshal Raj <veshalraj1307@gmail.com>
2024-04-08 17:56:06 +0530

    feat: added success, cancel, subscription route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AuthorizedOnly from "./routes/AuthorizedOnly";
 import AdminOnly from "./routes/AdminOnly";
 import UnAuthorizedOnly from "./routes/UnAuthorizedOnly";
+import SubscriptionPage from "./pages/SubscriptionPage";
+import SuccessPage from "./pages/SuccessPage";
+import PaymentError from "./pages/PaymentError";
 
 const queryClient = new QueryClient()
 
@@ -14,8 +17,11 @@ function App() {
         <BrowserRouter>
           <Routes>
               <Route path="/*" element={<UnAuthorizedOnly />} />
+              <Route path="/success" element={<SuccessPage />} />
+              <Route path="/cancel" element={<PaymentError />} />
               <Route path="/user/*" element={<AuthorizedOnly />} />
-              <Route path="/admin/*" element={<AdminOnly />} />           
+              <Route path="/admin/*" element={<AdminOnly />} />     
+              <Route path="/subscription" element={<SubscriptionPage   />} />      
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
