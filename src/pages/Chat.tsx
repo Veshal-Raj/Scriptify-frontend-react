59cd1a69ed2132212cd682e5a9548d0c39b361e9
Veshal Raj <veshalraj1307@gmail.com>
2024-04-08 18:02:15 +0530

    feat: added chat page

---
+++ src/pages/Chat.tsx
@@ -0,0 +1,17 @@
+
+import ChatBody from '../components/Chat/ChatBody'
+
+
+const Chat = () => {
+  return (
+    <>
+        <div className='bg-[#F5F7FB]'>
+
+        <ChatBody />
+        </div>
+        
+    </>
+  )
+}
+
+export default Chat
