b0bb5e7a887ca7497232e31f72db26808d8c2a23
Veshal Raj <veshalraj1307@gmail.com>
2024-04-12 17:27:54 +0530

    update: added some dummy data

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -11,3 +11,53 @@ export const benefits = [
 ];
 
 export const personAvatarUrl = 'https://source.unsplash.com/100x100/?person';
+
+
+export const users = [
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
+  // Add more users as needed
+];
+
+export const dummyData = [
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there!', time: '11:30 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+  { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
+
+  // Add more dummy conversations as needed
+];
+
