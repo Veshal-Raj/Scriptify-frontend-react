81685bb5fad2e3aa9407ed7872333850ce895023
Veshal Raj <veshalraj1307@gmail.com>
2024-04-12 17:27:05 +0530

    feat: created chatslice

---
+++ src/redux/slice/chatSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+    selectedUser: null
+};
+
+
+const chatSlice = createSlice({
+    name: "chat",
+    initialState,
+    reducers: {
+        setSelectedUser: (state, action) => {
+            state.selectedUser = action.payload
+        }
+    }
+})
+
+export const { setSelectedUser } = chatSlice.actions
+
+export default chatSlice.reducer;
