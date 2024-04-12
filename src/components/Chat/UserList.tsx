88a4f0c78336ecf4d848379ed4624cf5b3e5d1bf
Veshal Raj <veshalraj1307@gmail.com>
2024-04-12 17:25:16 +0530

    update: made userlist dynamic

--- src/components/Chat/UserList.tsx
+++ src/components/Chat/UserList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
@@ -7,45 +7,67 @@ import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 import Badge from '@mui/material/Badge';
 import { useMediaQuery } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
-// Placeholder profile image URL
-const placeholderImageURL = 'https://via.placeholder.com/150';
+import { fetchAllUsersApi } from '../../api/chat';
+import {  IUserList } from '../../@types/Tchat';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedUser } from '../../redux/slice/chatSlice';
+import { timeAgo } from '../../hooks/useDate';
 
-const users = [
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+type UserClickHandler = (user: IUserList) => void;
 
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
+interface UserListProps {
+  onUserClick: UserClickHandler;
+}
 
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
 
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 1, name: 'User 1', lastMessage: 'Hello', time: '10:00 AM',online: true },
 
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  { id: 2, name: 'User 2', lastMessage: 'Hi there!', time: '11:30 AM', online: false  },
-  // Add more users as needed
-];
 
-const UserList = ({ onUserClick}) => {
+const UserList: React.FC<UserListProps> = ({ onUserClick}) => {
+  const dispatch = useDispatch()
   const isSmallScreen = useMediaQuery('(max-width:600px)');
   const maxHeight = isSmallScreen ? 'auto' : '655px';
+  const [userListData, setUserListData ] = useState<IUserList[]>([])
+  const { userData } = useSelector(state => state.user)
+
+  const userId = userData._id
+
+  const { data: allUserForList, refetch } = useQuery({
+    queryKey: ['fetchAllUserForChatList'],
+    queryFn: fetchAllUsersApi,
+    enabled: false, // Disable auto-fetching on mount
+  });
+
+  useEffect(() => {
+    if (allUserForList?.data && allUserForList.data.response ) {
+      
+      const filteredUsers = allUserForList.data.response.filter((user: { userId: string; }) => user.userId !== userId);
+      setUserListData(filteredUsers);
+    }
+  }, [allUserForList, userId])
+
+  
+  useEffect(() => {
+    // Call the fetch function when the component mounts
+    refetch();
+  }, [refetch]);
+  
+  // console.log('all users ---- ',  userListData)
+
+  const handleUserClick = (user: IUserList) => {
+    console.log('user -||-- ', user)
+    dispatch(setSelectedUser(user))
+    
+    onUserClick(user.userId)
+  }
+
   return (
     <div className='my-4 px-4 ' style={{ maxHeight:  maxHeight, overflowY: 'auto' }}>
       <Stack spacing={2} >
         <List style={{ padding: 0 }}>
-          {users.map((user, index) => (
-            <ListItem key={index} alignItems="flex-start" style={{ border: '1px solid #ccc', display: 'flex', justifyContent: 'space-between', borderRadius: '10px', marginTop: '5px' }} onClick={() => onUserClick(user)}>
+          { userListData && userListData?.map((user) => (
+            <ListItem key={user.userId} alignItems="flex-start" style={{ border: '1px solid #ccc', display: 'flex', justifyContent: 'space-between', borderRadius: '10px', marginTop: '5px' }} onClick={() => handleUserClick(user)}>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <ListItemAvatar>
                   <Badge
@@ -59,15 +81,16 @@ const UserList = ({ onUserClick}) => {
                     invisible={!user.online}
                     
                   >
-                    <Avatar alt={user.name} src={placeholderImageURL} />
+                    <Avatar alt={user.username} src={user.profileImage} />
                   </Badge>
                 </ListItemAvatar>
                 <ListItemText
-                  primary={user.name}
-                  secondary={user.lastMessage.length > 15 ? user.lastMessage.substring(0, 15) + '...' : user.lastMessage}
+                  primary={user.username}
+                  secondary={user.lastMessage && user.lastMessage.length > 15 ? user.lastMessage.substring(0, 15) + '...' : user.lastMessage}
                 />
               </div>
-              <div style={{ color: 'green' }}>{user.time}</div>
+              <div style={{ color: 'green' }}>
+                {new Date(user.time).toLocaleString()}</div>
             </ListItem>
           ))}
         </List>
