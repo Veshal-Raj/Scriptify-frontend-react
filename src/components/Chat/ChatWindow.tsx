1f881deec0bbefeb12792c66337f2f87083b2c6a
Veshal Raj <veshalraj1307@gmail.com>
2024-04-12 17:23:07 +0530

    update: added socket implementation

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -3,69 +3,95 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import { Box } from '@mui/material';
+import { IConversation } from '../../@types/Tchat';
+import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { timeAgo } from '../../hooks/useDate';
 
-const dummyData = [
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 2, sender: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, receiver: { id: 1, name: 'John', image: 'sender1.jpg' }, message: 'Hi! How are you?', time: '11:35 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
-    { id: 1, sender: { id: 1, name: 'John', image: 'sender1.jpg' }, receiver: { id: 2, name: 'Jane', image: 'receiver1.jpg' }, message: 'Hello there! ....................................kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllll-----------------------------------------------------------llllllllllllllllllllllllllllllllllllllllllllll ', time: '11:30 AM' },
 
-    // Add more dummy conversations as needed
-];
+interface Props {
+    conversationData: IConversation[];
+    selectedUserId: string;
+}
 
-const ChatWindow = () => {
+const ChatWindow: React.FC<Props> = ({ conversationData, selectedUserId }) => {
+    const messagesEndRef =  useRef<HTMLDivElement>(null);
+    const { userData } = useSelector(state => state.user)
+    const cData = conversationData[0]
+    const { selectedUser } = useSelector(state => state.chat)
+
+    console.log('select user from store in chatwindow --- ', selectedUser)
+    // if (cData?.receiver.id !== selectedUserId) return <></>
+   
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+
+    const userId = userData._id
+
+    // console.log('receiver id --> ', conversationData[0])
+    // console.log('selected user id -->',selectedUserId)
+
+    useEffect(()=>{
+        scrollToBottom()
+    }, [conversationData])
+
+    
+    // console.log('selected user from state --- ', sel)
+
+   
     return (
-        <Box className='bg-[#E1DFEA]  pt-5 pb-10' sx={{ overflowY: 'auto', maxHeight: 'calc(100vh - 130px)' }}>
-            {dummyData.map(conversation => (
-                <Grid key={conversation.id} container spacing={2} >
-                    {/* Sender's message */}
-                    <Grid item xs={12} container justifyContent={conversation.sender.id === 1 ? "flex-end" : "flex-start"} sx={{ padding: '5px' }}>
-                        <Grid item >
-                            <Box sx={{ padding: 1 }} >
-                                {conversation.sender.id === 2 ? <Avatar alt={conversation.sender.name} src={conversation.sender.image} /> : null}
-                            </Box>
-                        </Grid>
-                        <Grid item sx={{ maxWidth: '80%' }} >
-                            <Paper
-                                variant="outlined"
-                                sx={{
-                                    padding: '10px',
-                                    backgroundColor: conversation.sender.id === 1 ? '#0087FD' : '#E1E5EC',
-                                    wordWrap: 'break-word',
-                                    overflowWrap: 'break-word',
-                                    borderRadius: '10px', // All corners curved
-                                    borderTopRightRadius: conversation.sender.id === 1 ? '0px' : '10px', 
-                                    borderTopLeftRadius: conversation.sender.id === 2 ? '0px' : '10px', 
-                                }}
-                            >
-                                <Typography variant="body1" color={conversation.sender.id === 1 ? "white" : "black"}>{conversation.message}</Typography>
-                            </Paper>
-                            <div className={`flex ${conversation.sender.id === 1 ? 'justify-end' : 'justify-start'}`}>
-    <Typography variant="caption" color="textSecondary">{conversation.time}</Typography>
-</div>
-                        </Grid>
-                        <Grid item >
-                            <Box sx={{ padding: 1 }}>
-                                {conversation.sender.id === 1 ? <Avatar alt={conversation.sender.name} src={conversation.sender.image} /> : null}
-                            </Box>
+        <>
+            <Box className='bg-[#E1DFEA] h-[100%] pt-5 pb-10 relative' sx={{ overflowY: 'auto', maxHeight: 'calc(100vh - 130px)' }} >
+                {conversationData.map((conversation, index) => (
+                    <Grid key={index} container spacing={2} >
+
+                        {/* Sender's message */}
+                        <Grid item xs={12} container justifyContent={conversation.sender.id === userId ? "flex-end" : "flex-start"} sx={{ padding: '5px' }}>
+                            <Grid item >
+                                <Box sx={{ padding: 1 }} >
+                                    {conversation.sender.id !== userId && conversation.receiver && ( /* Add null check */
+                                        <Avatar src={conversation.receiver?.profile_img} />
+                                    )}
+                                </Box>
+                            </Grid>
+                            <Grid item sx={{ maxWidth: '80%' }} >
+                                <Paper
+                                    variant="outlined"
+                                    sx={{
+                                        padding: '10px',
+                                        backgroundColor: conversation.sender.id === userId ? '#0087FD' : '#E1E5EC',
+                                        wordWrap: 'break-word',
+                                        overflowWrap: 'break-word',
+                                        borderRadius: '10px', // All corners curved
+                                        borderTopRightRadius: conversation.sender.id === userId ? '0px' : '10px',
+                                        borderTopLeftRadius: conversation.sender.id !== userId ? '0px' : '10px',
+                                    }}
+                                >
+                                    <Typography variant="body1" color={conversation.sender.id === userId ? "white" : "black"}>{conversation.message}</Typography>
+                                </Paper>
+                                <div className={`flex ${conversation.sender.id === userId ? 'justify-end' : 'justify-start'}`}>
+                                    <Typography variant="caption" color="textSecondary">
+                                        {timeAgo(conversation.time)}
+                                    </Typography>
+                                </div>
+                            </Grid>
+                            <Grid item >
+                                <Box sx={{ padding: 1 }}>
+                                    {/** need to change the avatar logic, ( !== ) change this into ( === ) */}
+                                    {conversation.sender.id === userId ? <Avatar  src={conversation.sender.profile_img} /> : null}
+                                </Box>
+                            </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-            ))}
-        </Box>
+                ))}
+            <div ref={messagesEndRef} />
+            </Box>
+            {/** for rendering this window */}
+            <p className='hidden' >{selectedUserId}</p>
+        </>
     );
 };
 
