b52d4a89b8bf1ead4bbf87e359a2703af45593ac
Veshal Raj <veshalraj1307@gmail.com>
2024-04-17 21:38:39 +0530

    update: added resend otp logic

--- src/components/UI/Timer.tsx
+++ src/components/UI/Timer.tsx
@@ -1,5 +1,8 @@
 // Timer.tsx
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { resentOtpApi } from "../../api/user";
+import { toast } from "sonner";
 
 type Props = {
   seconds: number;
@@ -7,12 +10,30 @@ type Props = {
 };
 
 const Timer: React.FC<Props> = ({ seconds, handleResendOTP }: Props) => {
+  const [trigger, setTrigger] = useState(false)
+
+  const { data: resendOtp, isLoading }= useQuery({
+    queryKey: ['resendOtp'],
+    queryFn: resentOtpApi,
+    enabled: trigger
+  })
+
+  const handleResendClick = () => {
+    console.log("Resend OTP"); // Log "Resend OTP" when button is clicked
+    setTrigger(true)
+    // console.log( 'resend otp --- ', )
+    const message = resendOtp?.data.response.message
+    if (message) toast(message)
+    
+    !isLoading ? handleResendOTP() : ''; // Call the provided handleResendOTP function
+  };
+
   return (
     <>
       {seconds > 0 ? (
         <h1 className="mt-10">Timer: {seconds}</h1>
       ) : (
-        <button className="mt-10" onClick={handleResendOTP}>
+        <button className="mt-10" onClick={handleResendClick}>
           Resend OTP
         </button>
       )}
