79251f2cdd1b5494160eed18c590b43e146be992
Veshal Raj <veshalraj1307@gmail.com>
2024-04-17 21:41:48 +0530

    feat: added forgot password and change password

--- src/routes/UnAuthorizedOnly.tsx
+++ src/routes/UnAuthorizedOnly.tsx
@@ -3,6 +3,8 @@ import Login from '../pages/Login'
 import Signup from "../pages/signup/Signup";
 import LandingPage from "../pages/LandingPage";
 import NotUserRoutes from "./routeComponents/NotUserRoutes";
+import ForgotPassword from "../pages/ForgotPassword";
+import ChangePasswordNotLogin from "../pages/ChangePasswordNotLogin";
 
 const UnAuthorizedOnly = () => {
   return (
@@ -12,6 +14,8 @@ const UnAuthorizedOnly = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/sign-in" element={<Login />} />
           <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/change-password" element={<ChangePasswordNotLogin />} />
         </Route>
       </Routes>
     </>
