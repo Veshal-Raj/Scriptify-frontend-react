5afedf3dd01a2cad63a296684919bfbe9fa64abd
Veshal Raj <veshalraj1307@gmail.com>
2024-04-19 23:37:17 +0530

    update: added googleAuth logic

--- src/components/UI/GoogleAuthButton.tsx
+++ src/components/UI/GoogleAuthButton.tsx
@@ -1,7 +1,64 @@
+import {GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import {app} from '../../utils/firebase' 
 import { Button } from "@mui/material"
 import GoogleIcon from '@mui/icons-material/Google';
+import { toast } from 'sonner';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { googleAuthUserApi } from '../../api/user';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../redux/slice/userSlice';
+import { useNavigate } from 'react-router-dom';
+
+interface UserData {
+  uid: string;
+}
 
 const GoogleAuthButton = () => {
+  const [googleUid, setGoogleUid] = useState<UserData>({ uid: '' })
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { mutate: googleAuthUserData } = useMutation({
+    mutationKey: ['googleAuth'],
+    mutationFn: googleAuthUserApi,
+    onSuccess: (response) => {
+      console.log(response)
+      if (response.status === 200) {
+        dispatch(setUser(response.data.response))
+        
+        toast.success('login successfull!')
+        if(response.data?.response.role === 'admin') setTimeout(() => navigate('/admin/dashboard'), 800)
+        if (response.data?.response.role === 'user') setTimeout(()=> navigate('/user/feed'),800)  
+      }
+    }
+  })
+
+
+  const handleGoogleClick = async() => {
+    try {
+        const provider = new GoogleAuthProvider()
+
+        const auth = getAuth(app)
+        const result = await signInWithPopup(auth, provider)
+
+        console.log('result --- ', result)
+        console.log('result --- ', result.user.uid)
+        const data: UserData = {
+          uid: result.user.uid
+        }
+      if (result.user.uid) {
+        setGoogleUid(data)
+    googleAuthUserData(data)
+
+      }
+
+    } catch (error) {
+      console.error('could not login with google', error);
+      toast.error(`Error: ${(error as Error).toString()}`)
+    }
+  }
+
   return (
     <div>
       <Button 
@@ -19,6 +76,7 @@ const GoogleAuthButton = () => {
             color: 'black',
           },
         }}
+        onClick={handleGoogleClick}
       >
         <GoogleIcon /> 
         Sign in with Google
