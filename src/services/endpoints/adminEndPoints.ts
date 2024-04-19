93aca1e4a176e0372c00fc2bbe9a2d795f162961
Veshal Raj <veshalraj1307@gmail.com>
2024-04-19 23:40:04 +0530

    feat: added getAllBlogs, changeBlogStatus, getAllReports

--- src/services/endpoints/adminEndPoints.ts
+++ src/services/endpoints/adminEndPoints.ts
@@ -1,6 +1,10 @@
 const adminRoutes = {
     getAllUsers: '/admin/getAllUsers',
     changeUserStatus:'/admin/changeUserStatus',
+    getAllBlogs: '/admin/getAllBlogs',
+    changeBlogStatus: '/admin/changeBlogStatus',
+    getAllReports: '/admin/getAllReports',
+    
 }
 
 export default adminRoutes;
