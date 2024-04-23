a5d91c1db683a7cbe608a4a22b341f85b8aa1b71
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:13:36 +0530

    refactor: reduced some codes

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -1,12 +1,8 @@
-import { Box,  Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useState } from "react";
 import CustomButton from "./Button/CustomButton";
-
-const imageVariants = {
-  initial: { opacity: 0, scale: 0, x: "0%", y: "0%" },
-  animate: { opacity: 1, scale: 1, x: ["0%", "0%", "0%"], y: ["0%", "0%", "-0%"], transition: { duration: 1.9 } },
-};
+import { imageVariants, landingImage1, landingImage2, landingImage3 } from "../utils/constants/constants";
 
 const LandingHero = () => {
 
@@ -26,10 +22,10 @@ const LandingHero = () => {
         } else {
           currentWordIndex = (currentWordIndex + 1) % words.length;
           currentCharacterIndex = 0;
-          clearInterval(interval); 
+          clearInterval(interval);
           setAnimationShown(true);
         }
-      }, 300); 
+      }, 300);
 
       return () => clearInterval(interval);
     }
@@ -37,16 +33,13 @@ const LandingHero = () => {
 
   return (
     <>
-      <Box sx={{ height: "auto", padding: "100px 100px", color: "#000",
-          "@media (max-width: 600px)": { padding: "40px"},         
-        }}
-      > 
+      <Box sx={{height: "auto", padding: "100px 100px", color: "#000", "@media (max-width: 600px)": { padding: "40px" }}} >
         <AnimatePresence>
           <Typography variant="h2" component="h2"
             sx={{
               padding: '10px',
               '@media (max-width: 600px)': {
-                fontSize: '2.5rem', // Adjust the font size for small screens
+                fontSize: '2.5rem', 
               },
             }}
           >
@@ -56,60 +49,40 @@ const LandingHero = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1 }}
-            
+
           >
-            <Typography
-  variant="h4"
-  component="h2"
-  sx={{
-    padding: '10px',
-    '@media (max-width: 600px)': {
-      fontSize: '1.5rem', // Adjust the font size for small screens
-    },
-  }}
->
-  Bringing Together Developers and Community: Where Ideas Flourish and Innovation Thrives!
-</Typography>
+            <Typography variant="h4" component="h2" 
+              sx={{
+                padding: '10px',
+                '@media (max-width: 600px)': {
+                  fontSize: '1.5rem',
+                },
+              }}
+            >
+              Bringing Together Developers and Community: Where Ideas Flourish and Innovation Thrives!
+            </Typography>
           </motion.div>
         </AnimatePresence>
-          <CustomButton to="/sign-in">Join the Community</CustomButton>
+        <CustomButton to="/sign-in">Join the Community</CustomButton>
       </Box>
       <div className="flex relative  overflow-hidden  bg-gradient-to-t from-white bg-opacity-5 ">
         <motion.div className="absolute top-24 -left-20 right-0 bottom-0 w-1/2 z-10  overflow-hidden hidden md:block lg:block "
           variants={imageVariants} initial="initial" animate="animate"
           whileHover={{ scale: 0.9 }}
-          onHoverStart={() => { }}
-          onHoverEnd={() => { }}
         >
-          <img
-            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'}
-            alt={`Image 1`}
-            className='h-[700px] w-auto object-cover'
-          />
+          <img src={landingImage1} alt={`Image 1`} className='h-[700px] w-auto object-cover' />
         </motion.div>
         <motion.div
           className="abosulte z-1 left-10 right-0 top-0 bottom-0 mx-auto hidden md:block lg:block"
-          variants={imageVariants}
-          initial="initial"
-          animate="animate"
+          variants={imageVariants} initial="initial" animate="animate"
           whileHover={{ scale: 1, rotate: [0, -5, 5, -5, 0], transition: { duration: 0.6 } }}
         >
-          <img
-            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'}
-            alt={`Image 2`}
-            className='h-[700px] w-full object-cover '
-          />
+          <img src={landingImage2} alt={`Image 2`} className='h-[700px] w-full object-cover ' />
         </motion.div>
         <motion.div className="absolute top-24 -right-10 z-10 hidden md:block lg:block" variants={imageVariants} initial="initial" animate="animate"
           whileHover={{ scale: 0.9 }}
-          onHoverStart={() => { }}
-          onHoverEnd={() => { }}
         >
-          <img
-            src={'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020083770/e37d830e-e204-41a5-b869-844e8d6f92cf.png?auto=format,compress'}
-            alt={`Image 3`}
-            className='h-[700px] w-auto object-cover '
-          />
+          <img src={landingImage3} alt={`Image 3`} className='h-[700px] w-auto object-cover ' />
         </motion.div>
       </div>
       <div className=" bg-white bg-gradient-to-t from-white py-10"></div>
