e0e23bd3d384a554fdc57101599a273782bae6ef
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:20:53 +0530

    update: added more constants

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -61,3 +61,31 @@ export const dummyData = [
   // Add more dummy conversations as needed
 ];
 
+
+export const landingImage1 = 'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'
+export const landingImage2 = 'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020251641/6a7b21f7-ab01-4f24-9633-31b350fe702c.png?auto=format,compress'
+export const landingImage3 = 'https://cdn.hashnode.com/res/hashnode/image/upload/v1702020083770/e37d830e-e204-41a5-b869-844e8d6f92cf.png?auto=format,compress'
+
+export const imageVariants = {
+  initial: { opacity: 0, scale: 0, x: "0%", y: "0%" },
+  animate: { opacity: 1, scale: 1, x: ["0%", "0%", "0%"], y: ["0%", "0%", "-0%"], transition: { duration: 1.9 } },
+};
+
+
+export const LandingPageBlogs = [
+  {
+      title: "Introduction to Node.js",
+      description: "Discover Node.js, the backbone of modern web development. Unleash the power of server-side JavaScript effortlessly.",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png"
+  },
+  {
+      title: "How to do Indexing in MongoDB",
+      description: "Learn the ins and outs of MongoDB indexing. Optimize your database performance like a pro.",
+      imageUrl: "https://miro.medium.com/v2/resize:fit:828/format:webp/0*tdiQR1PX3wb-V_bG.jpg"
+  },
+  {
+      title: "Is Python Easy to learn?",
+      description: "Delve into Python's ease of learning. Unlock the simplicity of Python and conquer programming challenges with ease.",
+      imageUrl: "https://miro.medium.com/v2/resize:fit:828/format:webp/1*ycIMlwgwicqlO6PcFRA-Iw.png"
+  }
+];
