d9f5e2cfc8569b802f79f0dca75b247453e3e17f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:22:42 +0530

    refactor: removed unwanted lines

--- src/components/Chat/InputArea.tsx
+++ src/components/Chat/InputArea.tsx
@@ -8,17 +8,17 @@ import { toast } from 'sonner';
 import { useSelector } from 'react-redux';
 import { useMutation } from '@tanstack/react-query';
 import { sendChatApi } from '../../api/user';
-import {  INewConversation } from '../../@types/Tchat';
+import { INewConversation } from '../../@types/Tchat';
 import EmojiPicker from 'emoji-picker-react';
 
 
-const InputArea =  ({ setConversationData, socket, selectedUserId }) => {
+const InputArea = ({ setConversationData, socket, selectedUserId }) => {
   const [message, setMessage] = useState('')
-  const [showEmojiPicker, setShowEmojiPicker] = useState(false); 
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const selectedUser = useSelector((state) => state.chat.selectedUser);
   const { userData } = useSelector(state => state.user)
 
-  const { mutate:sendChat } = useMutation({
+  const { mutate: sendChat } = useMutation({
     mutationFn: sendChatApi,
     onError: (error) => {
       toast.error(error.message)
@@ -29,15 +29,15 @@ const InputArea =  ({ setConversationData, socket, selectedUserId }) => {
     setMessage(event.target.value);
   };
 
-  
-  const handleSendMessage =async () => {
+
+  const handleSendMessage = async () => {
     if (message.trim().length === 0) {
       toast.info('Type something...')
       return; // Prevent sending message if it's empty
     }
     const newConversation: INewConversation = {
-      sender: { id: userData._id, name: userData.personal_info.username, profile_img: userData.personal_info.profile_img},
-      receiver: { id: selectedUser.userId, name: selectedUser.username, profile_img: selectedUser.profileImage},
+      sender: { id: userData._id, name: userData.personal_info.username, profile_img: userData.personal_info.profile_img },
+      receiver: { id: selectedUser.userId, name: selectedUser.username, profile_img: selectedUser.profileImage },
       message,
       time: new Date()
     }
@@ -49,27 +49,17 @@ const InputArea =  ({ setConversationData, socket, selectedUserId }) => {
     setMessage('')
   }
 
-  useEffect(()=> {
-   selectedUser 
-  },[sendChat])
+  useEffect(() => {
+    selectedUser
+  }, [sendChat])
 
-  useEffect(()=>{
+  useEffect(() => {
     socket.on("receive_message", (data: any) => {
-      // console.log('receive_message -->> ', data)
-      console.log('==============================', data)
-      console.log(selectedUserId,'----', data.receiver.id)
-      console.log(selectedUserId,'----', data.sender.id) 
-       if (selectedUserId === data.sender.id) {
-
-        // console.log('selecteduser from state -- ', selectedUser)
-        // console.log('selecteduser form props -- ', selectedUserId)
-        // if (data.sender.id === selectedUser)
-          setConversationData((prevData: any) => [...prevData, data])
+      if (selectedUserId === data.sender.id) {
+        setConversationData((prevData: any) => [...prevData, data])
       }
-      
-
     })
-  },[socket])
+  }, [socket])
 
   const handleKeyPress = (event: { key: string; }) => {
     if (event.key === 'Enter') {
@@ -78,47 +68,39 @@ const InputArea =  ({ setConversationData, socket, selectedUserId }) => {
     }
   };
 
-  const handleEmojiClick = (event: any) => {
-    setMessage(prevmessage => prevmessage + event.emoji);
-  };
+  const handleEmojiClick = (event: any) => setMessage(prevmessage => prevmessage + event.emoji);
 
   return (
     <div className=' relative '>
-    <div className='absolute -translate-y-[485px]  '>
-
-    {showEmojiPicker && <EmojiPicker onEmojiClick={handleEmojiClick} />}
-    </div>
-
-    <Box
-      position="fixed"
-      bottom={0}
-      width="67%"
-      bgcolor="#F5F7FB"
-      padding={2}
-      marginRight={10}
-      border="1px solid #ccc"
-      display="flex"
-      alignItems="center"
-      borderRadius={1}
-      zIndex={1000} 
-    >
+      <div className='absolute -translate-y-[485px]  '>
+        {showEmojiPicker && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+      </div>
+      <Box
+        position="fixed"
+        bottom={0}
+        width="67%"
+        bgcolor="#F5F7FB"
+        padding={2}
+        marginRight={10}
+        border="1px solid #ccc"
+        display="flex"
+        alignItems="center"
+        borderRadius={1}
+        zIndex={1000}
+      >
         <EmojiEmotionsIcon color='primary' style={{ marginRight: '5px' }} onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
-
-      <TextField
-        label="Type something..."
-        variant="outlined"
-        fullWidth
-        // multiline  
-        // rows={3}   
-        value={message}
-        onChange={handleMessageChange}
-        onKeyDown={handleKeyPress}
-        // Add any other props you need for the TextField
-      />
-      <IconButton color="primary" aria-label="send" onClick={handleSendMessage}>
-        <SendIcon />
-      </IconButton>
-    </Box>
+        <TextField
+          label="Type something..."
+          variant="outlined"
+          fullWidth
+          value={message}
+          onChange={handleMessageChange}
+          onKeyDown={handleKeyPress}
+        />
+        <IconButton color="primary" aria-label="send" onClick={handleSendMessage}>
+          <SendIcon />
+        </IconButton>
+      </Box>
     </div>
   );
 };
