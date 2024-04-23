347eb711213d4d7f6356d4374fcabff1a01a151c
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:27:22 +0530

    feat: added NavLinkProps

--- src/components/UI/NavLink.tsx
+++ src/components/UI/NavLink.tsx
@@ -1,7 +1,18 @@
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const NavLink = ({ to, text, variant = 'outlined', backgroundColor = '#007bff', color = 'white', hoverBackgroundColor = 'white', hoverColor = '#007bff' }) => {
+// Define the type for the props
+interface NavLinkProps {
+  to: string;
+  text: string;
+  variant?: 'text' | 'outlined' | 'contained';
+  backgroundColor?: string;
+  color?: string;
+  hoverBackgroundColor?: string;
+  hoverColor?: string;
+ }
+
+const NavLink: React.FC<NavLinkProps> = ({ to, text, variant = 'outlined', backgroundColor = '#007bff', color = 'white', hoverBackgroundColor = 'white', hoverColor = '#007bff' }) => {
     return (
       <Link to={to} style={{ textDecoration: 'none' }} className={text === 'Sign Up' ? 'block' : 'hidden md:block lg:block'}>
         <Button
