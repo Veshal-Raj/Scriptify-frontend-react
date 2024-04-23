52edf0e299bc80adb5cbf7e514380f1e1d343662
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:26:04 +0530

    refactor: removed unwanted line

--- src/components/Skeleton/SingleBlogSkeleton.tsx
+++ src/components/Skeleton/SingleBlogSkeleton.tsx
@@ -2,22 +2,21 @@ import { Skeleton } from '@mui/material'
 
 const SingleBlogSkeleton = () => {
   return (
-    
     <div className="max-w-[900px] block mx-auto py-10 max-lg:px-[5vw]">
-                <Skeleton variant="rectangular" width={600} height={300}  animation="wave" />
-                <div className="mt-12">
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="50%" animation="wave" />
-                    <Skeleton variant="text" width="20%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                    <Skeleton variant="text" width="70%" animation="wave" />
-                </div>
-            </div>
+      <Skeleton variant="rectangular" width={600} height={300} animation="wave" />
+      <div className="mt-12">
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="50%" animation="wave" />
+        <Skeleton variant="text" width="20%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+        <Skeleton variant="text" width="70%" animation="wave" />
+      </div>
+    </div>
   )
 }
 
