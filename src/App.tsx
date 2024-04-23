b4ba3103c2b76851f4a6b13f8aa64d361ec846ac
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 14:09:58 +0530

    update: implemented route specific lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import AuthorizedOnly from "./routes/AuthorizedOnly";
-import AdminOnly from "./routes/AdminOnly";
-import UnAuthorizedOnly from "./routes/UnAuthorizedOnly";
-import SubscriptionPage from "./pages/SubscriptionPage";
-import SuccessPage from "./pages/SuccessPage";
-import PaymentError from "./pages/PaymentError";
+import React, { Suspense } from "react";
+
+const AuthorizedOnly = React.lazy(() => import("./routes/AuthorizedOnly"))
+const AdminOnly = React.lazy(() => import("./routes/AdminOnly"))
+const UnAuthorizedOnly = React.lazy(() => import("./routes/UnAuthorizedOnly"))
+const SuccessPage  = React.lazy(() => import("./pages/SuccessPage"))
+const PaymentError = React.lazy(() => import("./pages/PaymentError"))
+const SubscriptionPage = React.lazy(() => import("./pages/SubscriptionPage"))
 
 const queryClient = new QueryClient()
 
@@ -16,12 +18,12 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-              <Route path="/*" element={<UnAuthorizedOnly />} />
-              <Route path="/success" element={<SuccessPage />} />
-              <Route path="/cancel" element={<PaymentError />} />
-              <Route path="/user/*" element={<AuthorizedOnly />} />
-              <Route path="/admin/*" element={<AdminOnly />} />     
-              <Route path="/subscription" element={<SubscriptionPage   />} />      
+              <Route path="/*" element={<Suspense fallback={<div>Loading...</div>}><UnAuthorizedOnly /></Suspense>} />
+              <Route path="/success" element={<Suspense fallback={<div>Loading...</div>}><SuccessPage /></Suspense>} />
+              <Route path="/cancel" element={<Suspense fallback={<div>Loading...</div>}><PaymentError /></Suspense>} />
+              <Route path="/user/*" element={<Suspense fallback={<div>Loading...</div>}><AuthorizedOnly /></Suspense>} />
+              <Route path="/admin/*" element={<Suspense fallback={<div>Loading...</div>}><AdminOnly /></Suspense>}/>
+              <Route path="/subscription" element={<Suspense fallback={<div>Loading...</div>}><SubscriptionPage   /></Suspense>} />      
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
