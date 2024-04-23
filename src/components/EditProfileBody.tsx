cc012b42ec50765f994e2d1de345e3f1a0975bdd
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:11:32 +0530

    refactor: removed some code

--- src/components/EditProfileBody.tsx
+++ src/components/EditProfileBody.tsx
@@ -10,6 +10,7 @@ import { setUser } from "../redux/slice/userSlice";
 import { toast } from "sonner";
 import { AxiosError } from "axios";
 
+
 const EditProfileBody = () => {
     const { userData } = useSelector(state => state.user);
     const dispatch = useDispatch();
@@ -20,7 +21,7 @@ const EditProfileBody = () => {
     const [editedSocialLinks, setEditedSocialLinks] = useState(socialLinks);
     const [editedPersonalInfo, setEditedPersonalInfo] = useState(personalInfo)
     const fileRef = useRef<HTMLInputElement>(null);
-    const [image, setImage] = useState(undefined)
+    const [image, setImage] = useState<File | undefined>(undefined);
     const [imagePercentage, setImagePercentage] = useState(0);
     const [imageError, setImageError] = useState(false);
     const [uploadedImage, setUploadedImage] = useState('')
@@ -28,44 +29,33 @@ const EditProfileBody = () => {
 
 
     useEffect(() => {
-        if (image) {
-            handleFileUpload(image)
-        }
+        if (image) handleFileUpload(image)
     }, [image])
 
     const { mutate: EditProfileData } = useMutation({
         mutationFn: EditProfileDataApi,
-        onMutate: ()=>{
-            setIsLoading(true)
-        },
-        onSuccess: (response)=> {
-            console.log('profile edit success')
-            console.log(response.data.response)
+        onMutate: () => setIsLoading(true),
+        onSuccess: (response) => {
             dispatch(setUser(response.data.response));
             setIsLoading(false)
             toast.success('Profile edited successfully')
         },
-        onError: (error: AxiosError | unknown ) => {
-            console.log(error)
+        onError: (error: AxiosError | unknown) => {
             setIsLoading(false)
             if (error instanceof AxiosError) {
                 const message = error.response?.data?.error as string
                 toast.error(message)
-            } else {
-                toast.error((error as Error).message || 'An unknown error occurred')
-            }
+            } else toast.error((error as Error).message || 'An unknown error occurred')
         }
     })
 
     const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
         const { name, value } = e.target;
-        // toast.warning('hi')
         if (name === "username" || name === "email" || name === "bio") {
             if (value.length > 200) {
                 toast.warning('Bio must be maximum 200 characters');
                 return;
             }
-    
             setEditedPersonalInfo((prevState: Record<string, string>) => ({
                 ...prevState,
                 [name]: value.trim()
@@ -82,13 +72,11 @@ const EditProfileBody = () => {
         const files = e.target.files
         if (files) {
             const fileToUpload = files[0]
-            console.log('file upload...', fileToUpload)
             setImage(fileToUpload)
         }
     }
 
-    const handleFileUpload = async (image) => {
-        console.log('image --- ', image)
+    const handleFileUpload = async (image: File) => {
         const storage = getStorage(app)
         const fileName = new Date().getTime() + image.name;
         const storageRef = ref(storage, fileName)
@@ -98,53 +86,44 @@ const EditProfileBody = () => {
             "state_changed",
             (snapshot) => {
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log("Upload is " + progress + "% done");
                 setImagePercentage(Math.round(progress));
             },
             (error) => {
                 console.error(error);
-
                 setImageError(true);
             },
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    console.log('donwloading...', downloadURL)
-                    setUploadedImage(downloadURL)
-                })
-
-            }
+            () => getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setUploadedImage(downloadURL))
         )
-
     }
 
     const handleSave = () => {
-        // Combine updated personal info and social links
         const trimmedUsername = editedPersonalInfo.username.trim();
-    const trimmedEmail = editedPersonalInfo.email.trim();
-    console.log(editedPersonalInfo)
-    console.log(editedPersonalInfo.bio)
-    console.log(editedPersonalInfo.bio.length)
-    if (editedPersonalInfo.bio.length >200) {
-        toast.error('Bio must be maximum 200 characters')
-        return
-    }
-    if (trimmedUsername === "" || trimmedEmail === "") {
-        // Show error message or handle invalid input
-        console.error("Name and email cannot be empty");
-        toast.error("Name and email cannot be empty")
-        return;
-    }
+        const trimmedEmail = editedPersonalInfo.email.trim();
+        if (editedPersonalInfo.bio.length > 200) {
+            toast.error('Bio must be maximum 200 characters')
+            return
+        }
+        if (trimmedUsername === "" || trimmedEmail === "") {
+            toast.error("Name and email cannot be empty")
+            return;
+        }
         const updatedUserData = {
             personal_info: editedPersonalInfo,
             social_links: editedSocialLinks,
             ...(uploadedImage && { uploaded_image: uploadedImage }),
             userId
         };
-
-        // Logging updated user data
-        console.log('updated userData --- ',updatedUserData);
         EditProfileData(updatedUserData)
     }
+
+    const socialLinksTextFile = [
+        { label: "YouTube", name: "youtube", defaultValue: editedSocialLinks.youtube || 'https://youtube.com/username' },
+        { label: "Instagram", name: "instagram", defaultValue: editedSocialLinks.instagram || 'https://instagram.com/username' },
+        { label: "Facebook", name: "facebook", defaultValue: editedSocialLinks.facebook || 'https://facebook.com/username' },
+        { label: "Twitter", name: "twitter", defaultValue: editedSocialLinks.twitter || 'https://twitter.com/username' },
+        { label: "GitHub", name: "github", defaultValue: editedSocialLinks.github || 'https://github.com/username' },
+        { label: "Website", name: "website", defaultValue: editedSocialLinks.website || 'https://website.com' }
+    ];
 
     return (
         <Box p={4}>
@@ -153,7 +132,7 @@ const EditProfileBody = () => {
                     <Avatar
                         alt={editedPersonalInfo.username}
                         src={uploadedImage || editedPersonalInfo.profile_img}
-                        sx={{ width: 100, height: 100, cursor: 'pointer', borderRadius: '50%',  border: '2px solid #2E8B57'  }}
+                        sx={{ width: 100, height: 100, cursor: 'pointer', borderRadius: '50%', border: '2px solid #2E8B57' }}
                         onClick={() => { fileRef?.current?.click(); }}
                     />
                     <p className="text-sm self-center  text-center mx-auto">
@@ -172,91 +151,25 @@ const EditProfileBody = () => {
                 </label>
                 <input id="profileImage" ref={fileRef} type="file" accept=".png, .jpg, .jpeg, .webp" style={{ display: 'none' }} onChange={handleImage} />
                 <Box mt={2}>
-                    <TextField
-                        label="Username"
-                        variant="outlined"
-                        fullWidth
-                        name="username"
-                        defaultValue={editedPersonalInfo.username}
-                        onChange={handleChange}
-                        margin="normal"
+                    <TextField label="Username" variant="outlined" fullWidth name="username"
+                        defaultValue={editedPersonalInfo.username} onChange={handleChange} margin="normal"
                     />
-                    <TextField
-                        label="Email"
-                        variant="outlined"
-                        fullWidth
-                        name="email"
-                        defaultValue={editedPersonalInfo.email}
-                        onChange={handleChange}
-                        margin="normal"
+                    <TextField label="Email" variant="outlined" fullWidth name="email"
+                        defaultValue={editedPersonalInfo.email} onChange={handleChange} margin="normal"
                     />
-                    <TextField
-                        label="Bio"
-                        variant="outlined"
-                        fullWidth
-                        name="bio"
-                        defaultValue={editedPersonalInfo.bio || 'Type your bio...'}
-                        onChange={handleChange}
-                        margin="normal"
+                    <TextField label="Bio" variant="outlined" fullWidth name="bio" 
+                        defaultValue={editedPersonalInfo.bio || 'Type your bio...'} 
+                        onChange={handleChange} margin="normal"
                     />
                     <Typography variant="h5" mb={2}>Social Links:</Typography>
                     <Box display="flex" flexDirection="column" alignItems="center" mb={4}>
-                        <TextField
-                            label="YouTube"
-                            variant="outlined"
-                            fullWidth
-                            name="youtube"
-                            defaultValue={editedSocialLinks.youtube || 'https://youtube.com/username'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Instagram"
-                            variant="outlined"
-                            fullWidth
-                            name="instagram"
-                            defaultValue={editedSocialLinks.instagram || 'https://instagram.com/username'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Facebook"
-                            variant="outlined"
-                            fullWidth
-                            name="facebook"
-                            defaultValue={editedSocialLinks.facebook || 'https://facebook.com/username'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Twitter"
-                            variant="outlined"
-                            fullWidth
-                            name="twitter"
-                            defaultValue={editedSocialLinks.twitter || 'https://twitter.com/username'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            label="GitHub"
-                            variant="outlined"
-                            fullWidth
-                            name="github"
-                            defaultValue={editedSocialLinks.github || 'https://github.com/username'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Website"
-                            variant="outlined"
-                            fullWidth
-                            name="website"
-                            defaultValue={editedSocialLinks.website || 'https://website.com'}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
+                        {socialLinksTextFile.map(link => (
+                            <TextField key={link.name} label={link.label} variant="outlined" fullWidth name={link.name}
+                                defaultValue={link.defaultValue} onChange={handleChange} margin="normal" />
+                        ))}
                         <Button variant="contained" disabled={isLoading} fullWidth onClick={handleSave} sx={{ bgcolor: '#007bff', color: "white", px: 2, py: 1, mx: 2, my: 1 }}>
-                            {isLoading ? <CircularProgress size={24} color="inherit" />: "Save"}</Button>
+                            {isLoading ? <CircularProgress size={24} color="inherit" /> : "Save"}
+                        </Button>
                     </Box>
                 </Box>
             </Box>
