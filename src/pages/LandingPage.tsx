5fa91dfb50afce29cea8e42f2d8661b441cc83ea
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 14:58:13 +0530

    update: implemented lazy loading

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react"
 import Navbar from "../components/Navbar"
 import LandingHero from "../components/LandingHero"
 import Footer  from "../components/Footer"
+import DefaultSkeletionPage from "../components/Skeleton/DefaultSkeletionPage"
 
 const LazyLandingSection1 =  lazy(() => import("../components/LandingSection1"))
 const LazyLandingSection2  = lazy(() => import("../components/LandingSection2" ))
@@ -12,7 +13,7 @@ const LandingPage = () => {
     <>
         <Navbar />
         <LandingHero />
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<DefaultSkeletionPage />}>
         <LazyLandingSection1 key="section1" />
         <LazyLandingSection2 key="section2" />
         <LazyLandingSection3 key="section3" />
