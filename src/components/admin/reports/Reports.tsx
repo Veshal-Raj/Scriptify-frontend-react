029c99f70b3620eb5d61ae1850c005f504d83038
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:31:15 +0530

    refactor: removed unwanted codes

--- src/components/admin/reports/Reports.tsx
+++ src/components/admin/reports/Reports.tsx
@@ -4,41 +4,17 @@ import ReportTable from "./ReportTable"
 import { getAllReportsApi } from "../../../api/admin";
 
 
-const dummyReports = [
-  {
-    _id: 1,
-    reporter: 'User A',
-    blogTitle: 'Blog Post 1',
-    reason: 'Inappropriate content',
-  },
-  {
-    _id: 2,
-    reporter: 'User B',
-    blogTitle: 'Blog Post 2',
-    reason: 'Spam',
-  },
-  {
-    _id: 3,
-    reporter: 'User C',
-    blogTitle: 'Blog Post 3',
-    reason: 'Violence',
-  },
-];
-
-
 const Reports = () => {
 
-  const {data: allReports} = useQuery({
+  const { data: allReports } = useQuery({
     queryKey: ['allReports'],
     queryFn: getAllReportsApi
   })
 
-  if (allReports?.data) console.log(allReports?.data.data)
-
   return (
     <>
-        <Navbar />
-        {allReports?.data.data ? <ReportTable reports={allReports?.data.data} /> : <></>}
+      <Navbar />
+      {allReports?.data.data ? <ReportTable reports={allReports?.data.data} /> : <></>}
     </>
   )
 }
