77cf4032e73181c02b2a60a3e288227b09f0983e
Veshal Raj <veshalraj1307@gmail.com>
2024-04-23 23:12:37 +0530

    update: added some props type

--- src/components/ForgotPasswordModal.tsx
+++ src/components/ForgotPasswordModal.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef, useState, ChangeEvent, KeyboardEvent, useCallback } from "react";
 import { useMutation } from "@tanstack/react-query";
-import { forgotPasswordOtpApi, verifyOTP } from "../api/user";
+import { forgotPasswordOtpApi } from "../api/user";
 import { useNavigate } from "react-router-dom";
 import BackdropLoading from "./UI/BackdropLoading";
-import { useDispatch } from "react-redux";
-import { setUser } from "../redux/slice/userSlice";
 import Timer from "./UI/Timer";
 import { AxiosError } from "axios";
 import { toast } from "sonner";
@@ -13,54 +11,47 @@ type Props = {
   length?: number;
   onOtpSubmit?: (otp: string) => void;
   email?: string;
+  setIsModalOpen?: (isOpen: boolean) => void;
 };
 
 const ForgotPasswordModal: React.FC<Props> = ({
-    setIsModalOpen,
+  setIsModalOpen,
   length = 4,
   onOtpSubmit = () => { },
 }: Props) => {
 
   const [sendOTP, setSendOTP] = useState('');
   const navigate = useNavigate()
-  const dispatch = useDispatch()
   const [otp, setOtp] = useState<string[]>(new Array(length).fill(""));
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false); // State for submission progress
   const inputRefs = useRef<HTMLInputElement[]>([]);
   const [seconds, setSeconds] = useState(60);
 
   useEffect(() => {
-    if (inputRefs.current[0]) {
-      inputRefs.current[0].focus();
-    }
+    if (inputRefs.current[0]) inputRefs.current[0].focus();
   }, []);
 
-    const { mutate: forgotPasswordOtp} = useMutation({
-      mutationFn: forgotPasswordOtpApi,
-      onSuccess: () => {
-        navigate('/change-password')
-      },
-      onError: (error: AxiosError | unknown ) => {
-        console.log('errror', error)
-        // setIsLoading(false)
-        if (error instanceof AxiosError) {
-            const message = error.response?.data?.error as string
-            if (message){
-                setIsModalOpen(false)
-                toast.error(message)
-            } else {
-                setIsModalOpen(false)
-                toast.error('OTP mismatch')
-                
-            }
+  const { mutate: forgotPasswordOtp } = useMutation({
+    mutationFn: forgotPasswordOtpApi,
+    onSuccess: () => navigate('/change-password'),
+    onError: (error: AxiosError | unknown) => {
+      if (error instanceof AxiosError) {
+        const message = error.response?.data?.error as string
+        if (message) {
+          if (setIsModalOpen) setIsModalOpen(false);
+          toast.error(message);
         } else {
-            setIsModalOpen(false)
-            toast.error((error as Error).message || 'An unknown error occurred')
+          if (setIsModalOpen) setIsModalOpen(false);
+          toast.error('OTP mismatch');
         }
+      } else {
+        if (setIsModalOpen) setIsModalOpen(false);
+        toast.error((error as Error).message || 'An unknown error occurred');
+      }
     }
-    })
+  })
 
-    const verifyOTPCallback = useCallback((combinedOtp: string) => {
+  const verifyOTPCallback = useCallback((combinedOtp: string) => {
     setIsSubmitting(true);
     const userOTP = { 'otp': combinedOtp };
     onOtpSubmit(combinedOtp);
@@ -69,26 +60,20 @@ const ForgotPasswordModal: React.FC<Props> = ({
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-      }
+      if (seconds > 0) setSeconds(seconds - 1);
     }, 1000);
 
-    return () => clearInterval(interval); // Cleanup function to clear interval
+    return () => clearInterval(interval); 
 
-  }, [seconds]); // Dependency array remains the same
+  }, [seconds]); 
 
   const handleChange = (index: number, e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     if (isNaN(Number(value))) return;
 
     const newOtp = [...otp];
-    // allow only one input
     newOtp[index] = value.substring(value.length - 1);
     setOtp(newOtp);
-
-
-    // submit trigger
     const combinedOtp = newOtp.join("");
     if (combinedOtp.length === length) {
       verifyOTPCallback(combinedOtp)
@@ -115,25 +100,13 @@ const ForgotPasswordModal: React.FC<Props> = ({
     }
   };
 
-  const handleKeyDown = (
-    index: number,
-    e: KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (
-      e.key === "Backspace" &&
-      !otp[index] &&
-      index > 0 &&
-      inputRefs.current[index - 1]
-    ) {
-      // Move focus to the previous input field on backspace
+  const handleKeyDown = ( index: number, e: KeyboardEvent<HTMLInputElement> ) => {
+    if ( e.key === "Backspace" && !otp[index] && index > 0 && inputRefs.current[index - 1] ) {      
       inputRefs.current[index - 1].focus();
     }
   };
 
-  const handleResendOTP = () => {
-    // send a resend otp req to the backend
-    setSeconds(60)
-  }
+  const handleResendOTP = () => setSeconds(60)
 
   return (
     <>
