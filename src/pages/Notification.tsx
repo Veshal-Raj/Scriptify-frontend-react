3f999ef26300bb6160e43b0aa21eab72c0e2ce0c
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:47:19 +0530

    update: fixed some bugs

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -8,46 +8,47 @@ import { fetchAllUserNotification } from "../api/user";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { timeAgo } from "../hooks/useDate";
+import { RootState } from "../redux/appStore";
 
 
 const Notification = () => {
     const isSmallScreen = useMediaQuery('(max-width:600px)');
     const [allNotificationData, setAllNotificationData] = useState([]);
     const [filterType, setFilterType] = useState('All');
-    
-    const { userData } = useSelector(state => state.user)
-  const userId = userData._id
 
-    const { data: AllUserNotification, isLoading, isError, refetch: refetchAllUserNotification } = useQuery({
+    const { userData } = useSelector((state: RootState) => state.user)
+    const userId = userData?._id
+
+    const { data: AllUserNotification, isLoading, refetch: refetchAllUserNotification } = useQuery({
         queryKey: ["initialAllUserNotification"],
-        queryFn: () => fetchAllUserNotification(userId),
-        
-    }) 
+        queryFn: async () => {
+            if (userId) {
+                const result = await fetchAllUserNotification(userId)
+                return result
+            } else {
+                return Promise.resolve({ data: '' });
+            }
+        }
+    })
 
     useEffect(() => {
-
         refetchAllUserNotification()
-        // if (!allNotificationData.length) refetchAllUserNotification()
-    },[])
+    }, [])
 
-    // console.log('all user notification --- ', AllUserNotification?.data.response)
     useEffect(() => {
         if (AllUserNotification?.data.response) {
             setAllNotificationData(AllUserNotification?.data.response);
         }
     }, [AllUserNotification]);
 
-    console.log('all notification -- ',allNotificationData)
-
     // Format notification data
-    const formatNotification = (data: { type: string; username: string; userImage: string; time: string | number | Date; userId: string; blogBannerImage: string; blogId: string; notificationId:string, seen: boolean }[]) => {
-        return data.map((item , index: number) => {
+    const formatNotification = (data: { type: string; username: string; userImage: string; time: string | number | Date; userId: string; blogBannerImage: string; blogId: string; notificationId: string, seen: boolean }[]) => {
+        return data.map((item, index: number) => {
             let text = "";
             let image = personAvatarUrl;
             let timestamp = "";
             let user_id = "";
             let blog_id = "";
-            // let type = ""
             const type = item.type;
             const notificationId = item.notificationId
             const Seen = item.seen
@@ -56,36 +57,27 @@ const Notification = () => {
                 case "follow":
                     text = ` ${item.username} followed you.`;
                     image = item.userImage || personAvatarUrl;
-                    timestamp =timeAgo(item.time);
+                    timestamp = timeAgo(item.time);
                     user_id = item.userId;
-                    // item.type
-                                 
                     break;
                 case "like":
                     text = `New like on your blog`;
                     image = item.blogBannerImage || personAvatarUrl;
-                    timestamp =timeAgo(item.time);
-                    blog_id = item.blogId;        
-                    // item.type                         
-
+                    timestamp = timeAgo(item.time);
+                    blog_id = item.blogId;
                     break;
                 case "comment":
                     text = `New comment on your blog`;
                     image = item.blogBannerImage || personAvatarUrl;
-                    timestamp =timeAgo(item.time);
-                    blog_id = item.blogId ;     
-                    // item.type                            
-
+                    timestamp = timeAgo(item.time);
+                    blog_id = item.blogId;                   
                     break;
                 case "save":
                     text = `Your blog have been saved`;
                     image = item.blogBannerImage || personAvatarUrl;
-                    timestamp =timeAgo(item.time);
-                    blog_id = item.blogId ;                                 
-                    // item.type
-
+                    timestamp = timeAgo(item.time);
+                    blog_id = item.blogId;
                     break;
-                // Add cases for other types if needed
                 default:
                     break;
             }
@@ -94,12 +86,9 @@ const Notification = () => {
     };
 
     const formattedNotifications = formatNotification(allNotificationData);
-    console.log(formattedNotifications);
 
-
-    
     const filters = ["All", "Like", "Comment", "Follow", "Save"];
-    console.log('filter type -- ', filterType)
+    
     return (
         <>
             <Navbar />
@@ -112,19 +101,19 @@ const Notification = () => {
             ) : (
                 <div className="flex justify-center">
                     <div className="m-2 p-2 flex gap-2 ">
-                    {filters.map(filter => (
-                        <Chip key={filter} label={filter} variant="outlined" color="primary" style={{ cursor: 'pointer' }} onClick={() => setFilterType(filter)} />
-                    ))}
+                        {filters.map(filter => (
+                            <Chip key={filter} label={filter} variant="outlined" color="primary" style={{ cursor: 'pointer' }} onClick={() => setFilterType(filter)} />
+                        ))}
                     </div>
                 </div>
             )}
             {isSmallScreen ? (
                 <div className="m-2 p-2" >
-                    <NotificationBody notifications={formattedNotifications} isLoading={isLoading} filterType={filterType}/>
+                    <NotificationBody notifications={formattedNotifications} isLoading={isLoading} filterType={filterType} />
                 </div>
             ) : (
                 <div className="flex justify-center">
-                    <NotificationBody notifications={formattedNotifications} isLoading={isLoading} filterType={filterType}/>
+                    <NotificationBody notifications={formattedNotifications} isLoading={isLoading} filterType={filterType} />
                 </div>
             )}
             <MobileFooter icon="notifications" />
