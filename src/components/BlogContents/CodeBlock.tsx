538e6ea05de2653ea5d4dbc53869511c0128f3a1
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:39:36 +0530

    update: added CodeBlockProps

--- src/components/BlogContents/CodeBlock.tsx
+++ src/components/BlogContents/CodeBlock.tsx
@@ -1,6 +1,8 @@
+interface CodeBlockProps {
+    code: string; // Assuming 'code' is a string. Adjust the type as necessary.
+   }
 
-
-const CodeBlock = ({ code }) => {
+const CodeBlock: React.FC<CodeBlockProps> = ({ code }) => {
     return (
         <pre className="bg-gray-100 p-4 rounded-md w-auto " style={{overflowX: 'auto'}}>
             <code>{code}</code>
