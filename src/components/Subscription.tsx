0ac38dc05a743ff64520166618e2d994acc3bda6
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:36:38 +0530

    update: added checkIcon props

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -2,7 +2,7 @@ import { Typography, Button, List, ListItem, ListItemText } from '@mui/material'
 import {motion} from 'framer-motion'
 import { useNavigate } from 'react-router-dom';
 
-function CheckIcon() {
+function CheckIcon({ sx }: { sx?: any }) {
   
   return (
     <svg
@@ -12,6 +12,7 @@ function CheckIcon() {
       strokeWidth={2}
       stroke="currentColor"
       className="h-3 w-3"
+      style={sx}
       >
       <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
     </svg>
@@ -19,15 +20,12 @@ function CheckIcon() {
 }
 
 export function PricingCard() {
-  const handleClick = () => {
-    navigate('/sign-in')
-  }
-
   const  navigate =  useNavigate()
+  const handleClick = () =>  navigate('/sign-in')
+
   return (
     <motion.div className="w-full max-w-[20rem] p-8 md:m-8 sm:mt-5 rounded-xl" style={{ background: 'linear-gradient(to right, #080a52, #0a0d6c)', transition: 'transform 0.3s' }}
-      whileHover={{scale: 1.05 }}
-      >
+      whileHover={{scale: 1.05 }}>
       <div className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center">
         <Typography variant="subtitle2" color="white" className="font-normal uppercase">
           Monthly
@@ -41,46 +39,41 @@ export function PricingCard() {
         <List className="flex flex-col gap-4">
           <ListItem className="flex items-center gap-4">
             <span className="rounded-full border border-white/20 bg-white/20 p-1">
-              <CheckIcon style={{ color: 'white' }} />
+              <CheckIcon sx={{ color: 'white' }} />
             </span>
-            <ListItemText primary={<Typography className="font-normal" style={{ color: 'white' }}>5 team members</Typography>} />
+            <ListItemText primary={<Typography className="font-normal" sx={{ color: 'white' }}>5 team members</Typography>} />
           </ListItem>
           <ListItem className="flex items-center gap-4">
             <span className="rounded-full border border-white/20 bg-white/20 p-1">
-              <CheckIcon style={{ color: 'white' }} />
+              <CheckIcon sx={{ color: 'white' }} />
             </span>
-            <ListItemText primary={<Typography className="font-normal" style={{ color: 'white' }}>200+ components</Typography>} />
+            <ListItemText primary={<Typography className="font-normal" sx={{ color: 'white' }}>200+ components</Typography>} />
           </ListItem>
           <ListItem className="flex items-center gap-4">
             <span className="rounded-full border border-white/20 bg-white/20 p-1">
-              <CheckIcon style={{ color: 'white' }} />
+              <CheckIcon sx={{ color: 'white' }} />
             </span>
-            <ListItemText primary={<Typography className="font-normal" style={{ color: 'white' }}>40+ built-in pages</Typography>} />
+            <ListItemText primary={<Typography className="font-normal" sx={{ color: 'white' }}>40+ built-in pages</Typography>} />
           </ListItem>
           <ListItem className="flex items-center gap-4">
             <span className="rounded-full border border-white/20 bg-white/20 p-1">
-              <CheckIcon style={{ color: 'white' }} />
+              <CheckIcon sx={{ color: 'white' }} />
             </span>
-            <ListItemText primary={<Typography className="font-normal" style={{ color: 'white' }}>1 year free updates</Typography>} />
+            <ListItemText primary={<Typography className="font-normal" sx={{ color: 'white' }}>1 year free updates</Typography>} />
           </ListItem>
           <ListItem className="flex items-center gap-4">
             <span className="rounded-full border border-white/20 bg-white/20 p-1">
-              <CheckIcon style={{ color: 'white' }} />
+              <CheckIcon sx={{ color: 'white' }} />
             </span>
-            <ListItemText primary={<Typography className="font-normal" style={{ color: 'white' }}>Life time technical support</Typography>} />
+            <ListItemText primary={<Typography className="font-normal" sx={{ color: 'white' }}>Life time technical support</Typography>} />
           </ListItem>
         </List>
       </div>
       <div className="mt-12 p-0">
-        <Button
-          variant="contained"
-          size="large"
+        <Button variant="contained" size="large"
           style={{ backgroundColor: '#080a52', color: 'white' }}
           className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
-          fullWidth={true}
-          onClick={handleClick}
-
-        >
+          fullWidth={true} onClick={handleClick}>
           Buy Now
         </Button>
       </div>
@@ -89,9 +82,7 @@ export function PricingCard() {
 }
 export function SecondPricingCard() {
   const  navigate =  useNavigate()
-  const handleClick = () => {
-    navigate('/sign-in')
-  }
+  const handleClick = () => navigate('/sign-in')
   return (
     <motion.div
       className="w-full max-w-[20rem] p-8 md:m-8 md:mt-[1.5rem] rounded-xl  "
@@ -142,18 +133,13 @@ export function SecondPricingCard() {
         </List>
       </div>
       <div className="mt-12 p-0">
-      <Button
-          variant="contained"
-          size="large"
+      <Button variant="contained" size="large"
           style={{ backgroundColor: '#080a52', color: 'white' }}
           className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
-          fullWidth={true}
-          onClick={handleClick}
-        >
+          fullWidth={true} onClick={handleClick} >
           Buy Now
         </Button>
       </div>
     </motion.div>
   );
 }
-
