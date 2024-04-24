e2719b40b683b031e3614c05425d91ec85cc5e72
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:41:03 +0530

    update: added props type

--- src/components/BlogContents/RawHTMLBlock.tsx
+++ src/components/BlogContents/RawHTMLBlock.tsx
@@ -1,6 +1,8 @@
+interface RawHTMLBlockProps {
+  code: string;
+ }
 
-
-const RawHTMLBlock = ({ code }) => {
+const RawHTMLBlock = ({ code }: RawHTMLBlockProps) => {
     return <pre dangerouslySetInnerHTML={{ __html: code }} style={{overflowX: 'auto'}} className="bg-black text-gray-200 p-5 rounded-lg"  />;
   };
 
