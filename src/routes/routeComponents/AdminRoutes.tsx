7481c624f29978773ea64bfea87da8f27da61764
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:52:48 +0530

    update: added props type

--- src/routes/routeComponents/AdminRoutes.tsx
+++ src/routes/routeComponents/AdminRoutes.tsx
@@ -1,9 +1,15 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 
+interface RootState {
+    user: {
+       userData: any;
+    };
+    
+   }
 
 const AdminRoutes = () => {
-    const { userData } = useSelector(state => state.user);
+    const { userData } = useSelector((state: RootState) => state.user);
 
     return userData?.role === 'admin' ? <Outlet /> : <Navigate to='/sign-in' />
 }
