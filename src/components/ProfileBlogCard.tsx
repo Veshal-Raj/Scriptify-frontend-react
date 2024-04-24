c3f5af9ffce9e4c7b092f156be3b2d57cf675a68
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:33:10 +0530

    update: added profileblogcard props

--- src/components/ProfileBlogCard.tsx
+++ src/components/ProfileBlogCard.tsx
@@ -5,7 +5,23 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { getDay } from "../hooks/useDate";
 import { useEffect } from "react";
 
-const ProfileBlogCard = ({ blog, index, username, ProfileId }) => {
+interface ProfileBlogCardProps {
+  blog: {
+     activity: {
+       total_likes?: number | undefined;
+     };
+     blog_id?: string | undefined;
+     title?: string | undefined;
+     banner?: string | undefined;
+     tags?: string[] | undefined;
+     publishedAt: string | number | Date;
+  };
+  index: number;
+  username?: string;
+  ProfileId?: string | undefined;
+ }
+
+const  ProfileBlogCard: React.FC<ProfileBlogCardProps> = ({ blog, index, username, ProfileId }) => {
   const { activity, blog_id, title, banner, tags, publishedAt } = blog;
   const { total_likes } = activity;
 
@@ -21,13 +37,11 @@ const ProfileBlogCard = ({ blog, index, username, ProfileId }) => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.5, delay: index * 0.3 }}
-    //   className="flex flex-col"
     >
       <Card className="w-full mb-0 flex justify-between p-2" variant="outlined" sx={{ borderRadius: '15px', maxWidth: '1000px', }}>
         <CardContent>
           <Link to={`/user/blog/${blog_id}`}>
             <div className="flex gap-2 items-center mb-7">
-              {/* <img src={avatar} alt={blog_id} className="w-6 h-6 rounded-full" /> */}
               <Typography variant="body1" component="p" className="line-clamp-1">{username}</Typography>
               <Typography variant="body1" component="p" className="min-w-fit">{getDay(publishedAt)}</Typography>
             </div>
@@ -35,7 +49,7 @@ const ProfileBlogCard = ({ blog, index, username, ProfileId }) => {
               <Typography variant="h5" className="text-2xl font-medium leading-7 line-clamp-3 sm:line-clamp-2 font-serif">{title}</Typography>
             </div>
             <div className="flex mt-7">
-              <Chip label={tags[0]} variant="outlined" color="primary" />
+              {tags && tags.length > 0 && <Chip label={tags[0]} variant="outlined" color="primary" />}
               <FavoriteBorderIcon className="text-xl ml-5 mr-3" />
               <Typography variant="body1" component="span" className="flex items-center text-gray-600 ">{total_likes}</Typography>
             </div>
