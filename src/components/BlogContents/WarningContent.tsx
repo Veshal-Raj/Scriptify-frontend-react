7a4e3080173b6f5d04a5d8a328ef1ca29ded5eb8
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:41:24 +0530

    update: added props type

--- src/components/BlogContents/WarningContent.tsx
+++ src/components/BlogContents/WarningContent.tsx
@@ -1,7 +1,11 @@
 import { Alert } from "@mui/material";
 
+interface WarningContentProps {
+    title: string;
+    message: string;
+   }
 
-const WarningContent = ({ title, message }) => {
+const WarningContent = ({ title, message }: WarningContentProps) => {
     return (
         <Alert severity="warning">
             <strong>{title}</strong> - {message}
