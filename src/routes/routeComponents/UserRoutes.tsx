8e02d4ab6b8ac8fa7e1e37f5057396d63f42a8af
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:53:16 +0530

    update: added props type

--- src/routes/routeComponents/UserRoutes.tsx
+++ src/routes/routeComponents/UserRoutes.tsx
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux";
 import {  Outlet, Navigate } from "react-router-dom";
 
+interface RootState {
+    user: {
+       userData: any;
+    };
+    
+   }
+
 export default function UserRoutes() {
     
-    const { userData } = useSelector(state => state.user);
+    const { userData } = useSelector((state: RootState) => state.user);
     console.log('----------------------',userData)
     
     return userData?.role === 'user' ? <Outlet /> : <Navigate to='/sign-in' />
