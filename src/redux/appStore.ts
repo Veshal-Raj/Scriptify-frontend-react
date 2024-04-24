9cb83c0c21cfabf6e9c04a694debdf12047ec30f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-24 23:51:27 +0530

    update: added RootState, Appdispatch

--- src/redux/appStore.ts
+++ src/redux/appStore.ts
@@ -31,3 +31,6 @@ export const store = configureStore({
 });
 
 export const persistor: Persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
